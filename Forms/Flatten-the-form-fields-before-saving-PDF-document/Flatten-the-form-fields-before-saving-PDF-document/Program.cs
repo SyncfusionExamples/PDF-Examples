196524d9f179065eab1009ca22f74ec8b2eba5d5
Sowmiya Loganathan <sowmiya.loganathan@syncfusion.com>
2022-09-12 17:00:35 +0530

    Added the topic "Flatten the form fields before saving the PDF document" in PDF forms

---
+++ Forms/Flatten-the-form-fields-before-saving-PDF-document/Flatten-the-form-fields-before-saving-PDF-document/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Syncfusion.Pdf.Parsing;
+using System.Reflection.Metadata;
+
+//Get stream from an existing PDF document. 
+FileStream docStream = new FileStream(Path.GetFullPath("../../../Input.pdf"), FileMode.Open, FileAccess.Read);
+
+//Load the PDF document.
+PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
+
+//Get the loaded form.
+PdfLoadedForm loadedForm = loadedDocument.Form;
+
+//Flatten the form fields.
+loadedForm.FlattenFields();
+
+//Create file stream.
+using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+{
+    //Save the PDF document to file stream.
+    loadedDocument.Save(outputFileStream);
+}
+
+//Close the document.
+loadedDocument.Close(true);
