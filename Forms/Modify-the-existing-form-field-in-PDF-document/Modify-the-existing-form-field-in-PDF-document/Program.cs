7229cc1ac0396f329f8099ea280d4be449a6fd9d
Sowmiya Loganathan <sowmiya.loganathan@syncfusion.com>
2022-09-12 15:19:53 +0530

    Add sample "Modify form fields in an existing PDF document" in GitHub

---
+++ Forms/Modify-the-existing-form-field-in-PDF-document/Modify-the-existing-form-field-in-PDF-document/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Syncfusion.Drawing;
+using Syncfusion.Pdf.Parsing;
+using System.Reflection.Metadata;
+
+//Get stream from an existing PDF document. 
+FileStream docStream = new FileStream(Path.GetFullPath("../../../Input.pdf"), FileMode.Open, FileAccess.Read);
+
+//Load the PDF document. 
+PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
+
+//Get the loaded form.
+PdfLoadedForm loadedForm = loadedDocument.Form;
+
+//Get the loaded form field and modify the properties.
+PdfLoadedTextBoxField loadedTextBoxField = loadedForm.Fields[0] as PdfLoadedTextBoxField;
+RectangleF newBounds = new RectangleF(100, 100, 150, 50);
+loadedTextBoxField.Bounds = newBounds;
+loadedTextBoxField.SpellCheck = true;
+loadedTextBoxField.Text = "New text of the field.";
+loadedTextBoxField.Password = false;
+
+//Create file stream.
+using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+{
+    //Save the PDF document to file stream.
+    loadedDocument.Save(outputFileStream);
+}
+
+//Close the document.
+loadedDocument.Close(true);
