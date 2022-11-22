2c4dcd0003a1d77ffd80f3bc77dfe849233022c6
Sowmiya Loganathan <sowmiya.loganathan@syncfusion.com>
2022-11-22 16:44:53 +0530

    Committed the modified sample

---
+++ OCR/.NET/Perform-OCR-an-image-and-convert-it-to-a-PDF-document/Perform-OCR-an-image-and-convert-it-to-a-PDF-document/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Syncfusion.OCRProcessor;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using System.Drawing;
+
+//Initialize the OCR processor by providing the path of the tesseract binaries.
+using (OCRProcessor processor = new OCRProcessor())
+{
+    //Get stream from an image file. 
+    FileStream imageStream = new FileStream(Path.GetFullPath(@"../../../Input.jpg"), FileMode.Open);    
+
+    //Set OCR language to process.
+    processor.Settings.Language = Languages.English;
+
+    //Sets Unicode font to preserve the Unicode characters in a PDF document.
+    FileStream fontStream = new FileStream(Path.GetFullPath(@"../../../ARIALUNI.ttf"), FileMode.Open);
+
+    //Set the unicode font. 
+    processor.UnicodeFont = new PdfTrueTypeFont(fontStream, true, PdfFontStyle.Regular, 10);
+
+    //Set the PDF conformance level.
+    processor.Settings.Conformance = PdfConformanceLevel.Pdf_A1B;
+
+    //Process OCR by providing the bitmap image.  
+    PdfDocument document = processor.PerformOCR(imageStream);
+
+    //Create file stream.
+    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+    {
+        //Save the PDF document to file stream.
+        document.Save(outputFileStream);
+    }
+
+    //Close the document.
+    document.Close(true);
+}
---
+++ OCR/.NET/Set-temp-folder-while-performing-OCR/Set-temp-folder-while-performing-OCR/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Syncfusion.OCRProcessor;
+using Syncfusion.Pdf.Parsing;
+
+string tesseractBinariesPath = Path.GetFullPath("../../../../../Tesseractbinaries/Windows");
+string tessdataPath = Path.GetFullPath("../../../../../Tessdata");
+
+//Initialize the OCR processor by providing the path of the tesseract.
+using (OCRProcessor processor = new OCRProcessor(tesseractBinariesPath))
+{
+    //Get stream from an existing PDF document. 
+    FileStream stream = new FileStream(Path.GetFullPath("../../../Input.pdf"), FileMode.Open);
+
+    //Load the PDF document. 
+    PdfLoadedDocument document = new PdfLoadedDocument(stream);
+
+    //Set OCR language to process.
+    processor.Settings.Language = Languages.English;
+
+    //Set custom temp file path location.
+    processor.Settings.TempFolder = Path.GetFullPath("D:/Temp/");
+
+    //Process OCR by providing the PDF document, data dictionary, and language.
+    processor.PerformOCR(document, tessdataPath);
+
+    //Create file stream.
+    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+    {
+        //Save the PDF document to file stream.
+        document.Save(outputFileStream);
+    }
+
+    //Close the document.
+    document.Close(true);
+}
