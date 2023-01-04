927f72f955b53f428c176c02a0f7609eb2f20a5a
jeyalakshmit <jeyalakshmi.t@syncfusion.com>
2023-01-04 12:19:33 +0530

    EJDOTNETCORE-4365 created a sample for .NET console application

---
+++ Getting Started/.NET/Create_PDF_NET/Create_PDF_NET/Program.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf;
+
+//Create a new PDF document.
+PdfDocument document = new PdfDocument();
+//Add a page to the document.
+PdfPage page = document.Pages.Add();
+//Create PDF graphics for the page.
+PdfGraphics graphics = page.Graphics;
+//Set the standard font.
+PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 20);
+//Draw the text.
+graphics.DrawString("Hello World!!!", font, PdfBrushes.Black, new Syncfusion.Drawing.PointF(0, 0));
+
+//Create file stream.
+using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+{
+    //Save the PDF document to file stream.
+    document.Save(outputFileStream);
+}
+//Close the document.
+document.Close(true);
---
+++ Getting Started/.NET/HTML_to_PDF_NET/HTML_to_PDF_NET/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Syncfusion.Pdf;
+using Syncfusion.HtmlConverter;
+
+//Initialize HTML to PDF converter.
+HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+//Convert URL to PDF document. 
+PdfDocument document = htmlConverter.Convert("https://www.google.com");
+
+//Create file stream.
+using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+{
+    //Save the PDF document to file stream.
+    document.Save(outputFileStream);
+}
+//Close the document.
+document.Close(true);
