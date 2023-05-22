c42df2f828f402289dc15dcc5634037d75c73f64
sameerkhan001 <sameerkhan.n@syncfusion.com>
2023-05-22 11:27:47 +0530

    828959 Added split Use case sample project.

---
+++ Split PDFs/Password-Protect-Split-PDF/Password-Protect-Split-PDF/Program.cs
@@ -0,0 +1,24 @@
+﻿using Syncfusion.Pdf.Parsing;
+using Syncfusion.Pdf;
+
+// Create the FileStream object. 
+using (FileStream inputFileStream = new FileStream("../../../Data/Input.pdf", FileMode.Open, FileAccess.Read))
+{
+    // Create a PdfLoadedDocument object from the FileStream object  
+    PdfLoadedDocument loadedDocument = new PdfLoadedDocument(inputFileStream,"password");
+    // Iterate over the pages in the PdfLoadedDocument object  
+    for (int pageIndex = 0; pageIndex < loadedDocument.PageCount; pageIndex++)
+    {
+        // Create a new PdfDocument object  
+        using (PdfDocument outputDocument = new PdfDocument())
+        {
+            // Import the page from the PdfLoadedDocument object to the PdfDocument object  
+            outputDocument.ImportPage(loadedDocument, pageIndex);
+            //Save the document into a filestream object. 
+            using (FileStream outputFileStream = new FileStream("../../../Output" + pageIndex + ".pdf", FileMode.Create, FileAccess.Write))
+            {
+                outputDocument.Save(outputFileStream);
+            }
+        }
+    }
+}
---
+++ Split PDFs/Rotate-PDF-and-Split/Rotate-PDF-and-Split/Program.cs
@@ -0,0 +1,24 @@
+﻿using Syncfusion.Pdf.Parsing;
+using Syncfusion.Pdf;
+
+//Load the PDF document.
+FileStream docStream = new FileStream("../../../Data/Input.pdf", FileMode.Open, FileAccess.Read);
+PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
+for (int pageIndex = 0;pageIndex<loadedDocument.PageCount; pageIndex++)
+{
+    //Gets the page
+    PdfPageBase loadedPage = loadedDocument.Pages[pageIndex] as PdfPageBase;
+    //Set the rotation for loaded page.
+    loadedPage.Rotation = PdfPageRotateAngle.RotateAngle90;
+    using (var outputDocument = new PdfDocument())
+    {
+        //Import the pages to the new PDF document.  
+        outputDocument.ImportPage(loadedDocument, pageIndex);
+        //Save the document into a filestream object. 
+        using (FileStream outputFileStream = new FileStream("../../../Output"+pageIndex+".pdf", FileMode.Create, FileAccess.Write))
+        {
+            outputDocument.Save(outputFileStream);
+        }
+    }
+}
+
