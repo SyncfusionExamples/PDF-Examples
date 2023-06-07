2e6c80ef122d6e9327f764f44ea404858d8c2d0a
jeyalakshmit <jeyalakshmi.t@syncfusion.com>
2023-06-07 15:22:26 +0530

    updated the Secured PDF samples

--- Compression/Compress_secured_PDF_NET/.NET-Standard/Compress_secured_PDF_NET/Program.cs
+++ Compression/Compress_secured_PDF_NET/.NET-Standard/Compress_secured_PDF_NET/Program.cs
@@ -6,18 +6,8 @@ namespace Compress_secured_PDF_NET {
     internal class Program {
         static void Main(string[] args) {
             //Load existing PDF document.
-            FileStream fileStream = new FileStream(Path.GetFullPath("../../../Data/imageDoc.pdf"), FileMode.Open, FileAccess.Read);
-            PdfLoadedDocument document = new PdfLoadedDocument(fileStream);
-
-            //Create a document security.
-            PdfSecurity security = document.Security;
-
-            //Set user password for the document.
-            security.UserPassword = "sample123";
-            //Set encryption algorithm.
-            security.Algorithm = PdfEncryptionAlgorithm.AES;
-            //Set key size.
-            security.KeySize = PdfEncryptionKeySize.Key256Bit;
+            FileStream fileStream = new FileStream(Path.GetFullPath("../../../Data/SecurePDF.pdf"), FileMode.Open, FileAccess.Read);
+            PdfLoadedDocument document = new PdfLoadedDocument(fileStream, "sample123");
 
             //Create a new compression option.
             PdfCompressionOptions options = new PdfCompressionOptions();
