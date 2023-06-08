0f807bd36b6eea87370f11e48386fd8ed8785069
sameerkhan001 <sameerkhan.n@syncfusion.com>
2023-06-08 11:02:26 +0530

    830751 Added Split method sample project

---
+++ Split PDFs/Split-PDF-based-Bookmarks/.NET/Split-PDF-based-Bookmarks/Program.cs
@@ -0,0 +1,37 @@
+﻿using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf.Parsing;
+using Syncfusion.Pdf;
+
+// Load the PDF document 
+using (FileStream fileStream = new FileStream("../../../Input.pdf", FileMode.Open, FileAccess.Read))
+using (PdfLoadedDocument loadedDocument = new PdfLoadedDocument(fileStream))
+{
+    PdfBookmarkBase bookmarks = loadedDocument.Bookmarks;
+    // Iterate all the bookmarks and their page ranges 
+    foreach (PdfBookmark bookmark in bookmarks)
+    {
+        if (bookmark.Destination != null)
+        {
+            if (bookmark.Destination.Page != null)
+            {
+                int endIndex = bookmark.Destination.PageIndex;
+                int count = 0;
+                // Create a new PDF document
+                using (PdfDocument document = new PdfDocument())
+                {
+                    foreach (PdfLoadedBookmark childBookmark in bookmark)
+                    {
+                        endIndex = childBookmark.Destination.PageIndex;
+                    }
+                    // Import the pages to the new PDF document 
+                    document.ImportPageRange(loadedDocument, bookmark.Destination.PageIndex, endIndex);
+                    //Save the document as stream
+                    using (FileStream stream = new FileStream(bookmark.Title +".pdf",FileMode.CreateNew,FileAccess.Write))
+                    {
+                        document.Save(stream);
+                    }
+                }
+            }
+        }
+    }
+}
