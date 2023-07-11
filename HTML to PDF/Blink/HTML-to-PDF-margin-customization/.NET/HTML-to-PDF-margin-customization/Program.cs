5bad51f91ed108527b79e352071c02ea7cf6309a
jeyalakshmit <jeyalakshmi.t@syncfusion.com>
2023-07-11 16:02:30 +0530

    Added the sample for HTML to PDF topics.

---
+++ HTML to PDF/Blink/HTML-to-PDF-margin-customization/.NET/HTML-to-PDF-margin-customization/Program.cs
@@ -0,0 +1,26 @@
+﻿using Syncfusion.HtmlConverter;
+using Syncfusion.Pdf;
+
+namespace HTML_to_PDF_margin_customization {
+    internal class Program {
+        static void Main(string[] args) {
+            //Initialize the HTML to PDF converter.
+            HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+            //Initialize blink converter settings. 
+            BlinkConverterSettings blinkConverterSettings = new BlinkConverterSettings();
+            //Set the margin.
+            blinkConverterSettings.Margin.All = 50;
+            //Assign Blink converter settings to HTML converter.
+            htmlConverter.ConverterSettings = blinkConverterSettings;
+            //Convert URL to PDF document.  
+            PdfDocument document = htmlConverter.Convert("https://www.google.com");
+
+            //Create a file stream.
+            FileStream fileStream = new FileStream("Output.pdf", FileMode.Create, FileAccess.ReadWrite);
+            //Save the PDF document to the file stream.
+            document.Save(fileStream);
+            //Close the document.
+            document.Close(true);
+        }
+    }
+}
