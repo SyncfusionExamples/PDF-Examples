ec5be6167613ab3016c4bcbd65af3eba5d239cf6
Sameerkhan <sameerkhan.n@syncfusion.com>
2023-07-21 10:46:48 +0530

    839592 Added AWS sample project in Github PDF Example branch

---
+++ Open and Save PDF document/AWS/AWSLambdaProject/AWSLambdaProject/Function.cs
@@ -0,0 +1,63 @@
+using Amazon.Lambda.Core;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Drawing;
+using System.IO;
+using Syncfusion.Pdf.Parsing;
+using Syncfusion.Pdf.Grid;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AWSLambdaProject;
+
+public class Function
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public string FunctionHandler(string input, ILambdaContext context)
+    {
+        string filePath = Path.GetFullPath(@"Data/Input.pdf");
+        //Open an existing PDF document.
+        using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        {
+            PdfLoadedDocument document = new PdfLoadedDocument(stream);
+            //Get the first page from a document.
+            PdfLoadedPage page = document.Pages[0] as PdfLoadedPage;
+            //Create PDF graphics for the page.
+            PdfGraphics graphics = page.Graphics;
+
+            //Create a PdfGrid.
+            PdfGrid pdfGrid = new PdfGrid();
+            //Add values to the list.
+            List<object> data = new List<object>();
+            Object row1 = new { Product_ID = "1001", Product_Name = "Bicycle", Price = "10,000" };
+            Object row2 = new { Product_ID = "1002", Product_Name = "Head Light", Price = "3,000" };
+            Object row3 = new { Product_ID = "1003", Product_Name = "Break wire", Price = "1,500" };
+            data.Add(row1);
+            data.Add(row2);
+            data.Add(row3);
+            //Add list to IEnumerable.
+            IEnumerable<object> dataTable = data;
+
+            //Assign data source.
+            pdfGrid.DataSource = dataTable;
+            //Apply built-in table style.
+            pdfGrid.ApplyBuiltinStyle(PdfGridBuiltinStyle.GridTable4Accent3);
+            //Draw the grid to the page of PDF document.
+            pdfGrid.Draw(graphics, new RectangleF(40, 400, page.Size.Width - 80, 0));
+            //Save the document into stream.
+            MemoryStream memoryStream = new MemoryStream();
+            //Save and Close the PDF Document.
+            document.Save(memoryStream);
+            document.Close(true);
+            return Convert.ToBase64String(memoryStream.ToArray());
+        }
+
+    }
+}
---
+++ Open and Save PDF document/AWS/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Amazon;
+using Amazon.Lambda;
+using Amazon.Lambda.Model;
+using Newtonsoft.Json;
+using System.IO;
+
+//Create a new AmazonLambdaClient
+AmazonLambdaClient client = new AmazonLambdaClient("awsaccessKeyID", "awsSecreteAccessKey", RegionEndpoint.USEast2);
+//Create new InvokeRequest with published function name.
+InvokeRequest invoke = new InvokeRequest
+{
+    FunctionName = "MyNewFunction",
+    InvocationType = InvocationType.RequestResponse,
+    Payload = "\"Test\""
+};
+//Get the InvokeResponse from client InvokeRequest
+InvokeResponse response = await client.InvokeAsync(invoke);
+//Read the response stream
+var stream = new StreamReader(response.Payload);
+JsonReader reader = new JsonTextReader(stream);
+var serilizer = new JsonSerializer();
+var responseText = serilizer.Deserialize(reader);
+//Convert Base64String into Word document
+byte[] bytes = Convert.FromBase64String(responseText.ToString());
+FileStream fileStream = new FileStream("Sample.pdf", FileMode.Create);
+BinaryWriter writer = new BinaryWriter(fileStream);
+writer.Write(bytes, 0, bytes.Length);
+writer.Close();
+System.Diagnostics.Process.Start("Sample.pdf");
