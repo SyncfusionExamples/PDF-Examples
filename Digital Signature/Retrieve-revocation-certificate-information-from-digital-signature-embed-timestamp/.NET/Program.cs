c25f56a3d6db71368f423c4b958c37b7547d02b2
Srihariharan <srihariharan.g@syncfusion.com>
2023-08-31 15:22:55 +0530

    845229_sample: UG Documentation works for Volume 3 features.

---
+++ Digital Signature/Retrieve-revocation-certificate-information-from-digital-signature-embed-timestamp/.NET/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Syncfusion.Pdf.Parsing;
+using Syncfusion.Pdf.Security;
+using System.Reflection.Metadata;
+//Load a PDF document
+FileStream docStream = new FileStream("../../../Input.pdf", FileMode.Open, FileAccess.Read);
+PdfLoadedDocument document = new PdfLoadedDocument(docStream);
+//Gets the signature field.
+PdfLoadedSignatureField signatureField = document.Form.Fields[0] as PdfLoadedSignatureField;
+//Validates signature and gets the validation result.
+PdfSignatureValidationResult result = signatureField.ValidateSignature();
+//Gets signer certificates
+PdfSignerCertificate[] certifcate = result.TimeStampInformation.SignerCertificates;
+//Close the document.
+document.Close(true);
