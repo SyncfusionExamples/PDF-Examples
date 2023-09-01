49feeaa639ef8c497af1f03172863e81482bc4a8
Srihariharan <srihariharan.g@syncfusion.com>
2023-09-01 12:03:08 +0530

    845229_sample : print the certificate value.

--- Digital Signature/Retrieve-revocation-certificate-information-from-digital-signature-embed-timestamp/.NET/Program.cs
+++ Digital Signature/Retrieve-revocation-certificate-information-from-digital-signature-embed-timestamp/.NET/Program.cs
@@ -12,9 +12,38 @@ PdfSignatureValidationResult result = signatureField.ValidateSignature();
 //Gets signer certificates
 PdfSignerCertificate[] certifcate = result.TimeStampInformation.SignerCertificates;
 //Print the certifcate value
-for(int i = 0;i < certifcate.Length; i++)
+foreach (PdfSignerCertificate signerCertificate in certifcate)
 {
-    Console.WriteLine(certifcate[i].Certificate);
+    if (signerCertificate.OcspCertificate != null)
+    {
+        Console.WriteLine("------------OCSP Certificate-------------");
+        Console.WriteLine();
+        foreach (X509Certificate2 item in signerCertificate.OcspCertificate.Certificates)
+        {
+            Console.WriteLine("The OCSP Response was signed by " + item.SubjectName.Name);
+        }
+        Console.WriteLine("Is Embedded: " + signerCertificate.OcspCertificate.IsEmbedded);
+        Console.WriteLine("ValidFrom: " + signerCertificate.OcspCertificate.ValidFrom);
+        Console.WriteLine("ValidTo: " + signerCertificate.OcspCertificate.ValidTo);
+        Console.WriteLine();
+        continue;
+    }
+    if (signerCertificate.CrlCertificate != null)
+    {
+        Console.WriteLine("------------CRL Certificate--------------");
+        Console.WriteLine();
+        foreach (X509Certificate2 item in signerCertificate.CrlCertificate.Certificates)
+        {
+            if (item != null)
+            {
+                Console.WriteLine("The CRL was signed by " + item.SubjectName.Name);
+            }
+        }
+        Console.WriteLine("Is Embedded: " + signerCertificate.CrlCertificate.IsEmbedded);
+        Console.WriteLine("ValidFrom: " + signerCertificate.CrlCertificate.ValidFrom);
+        Console.WriteLine("ValidTo: " + signerCertificate.CrlCertificate.ValidTo);
+        break;
+    }
 }
 //Close the document.
 document.Close(true);
