2a5d0c4ef90a8ef5931d17166cc9b0a561dc2206
jeyalakshmit <jeyalakshmi.t@syncfusion.com>
2023-09-20 15:41:29 +0530

    Added the sample-2023 Volume 3 Release

---
+++ Digital Signature/Get-images-from-the-existing-signed-signature-field/.NET/Get-images-from-the-existing-signed-signature-field/Program.cs
@@ -0,0 +1,21 @@
+﻿using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf.Parsing;
+
+namespace Get_images_from_the_existing_signed_signature_field {
+    internal class Program {
+        static void Main(string[] args) {
+            //Load an existing PDF file.
+            FileStream fileStream = new FileStream(Path.GetFullPath("../../../Input.pdf"), FileMode.Open, FileAccess.Read);
+            PdfLoadedDocument ldoc = new PdfLoadedDocument(fileStream);
+            //Get the existing signed signature field.
+            PdfLoadedSignatureField loadedSignature = ldoc.Form.Fields[0] as PdfLoadedSignatureField;
+            //Get the image streams.
+            Stream[] imageStreams = loadedSignature.GetImages();
+            for (int i = 0; i < imageStreams.Length; i++) {
+                File.WriteAllBytes("Output" + i.ToString() + ".jpg", (imageStreams[i] as MemoryStream).ToArray());
+            }
+            //Close a PDF document.
+            ldoc.Close(true);
+        }
+    }
+}
