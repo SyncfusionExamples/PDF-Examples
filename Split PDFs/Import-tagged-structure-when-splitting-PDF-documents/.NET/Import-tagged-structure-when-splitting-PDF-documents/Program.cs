69d3faa746d0abaef7c89d853ffdbfcfa8e14002
jeyalakshmit <jeyalakshmi.t@syncfusion.com>
2023-12-20 10:58:08 +0530

    Added the sample for Split PDFs

---
+++ Split PDFs/Import-tagged-structure-when-splitting-PDF-documents/.NET/Import-tagged-structure-when-splitting-PDF-documents/Program.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.Pdf.Parsing;
+using Syncfusion.Pdf;
+
+namespace Import_tagged_structure_when_splitting_PDF_documents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Load an existing PDF file.
+            PdfLoadedDocument loadDocument = new PdfLoadedDocument(new FileStream("../../../Input.pdf", FileMode.Open));
+            //Subscribe to the document split event.
+            loadDocument.DocumentSplitEvent += LoadDocument_DocumentSplitEvent;
+            void LoadDocument_DocumentSplitEvent(object sender, PdfDocumentSplitEventArgs args)
+            {
+                //Save the resulting document.
+                FileStream outputStream = new FileStream(Guid.NewGuid().ToString() + ".pdf", FileMode.CreateNew);
+                args.PdfDocumentData.CopyTo(outputStream);
+                outputStream.Close();
+            }
+            //Create the split options object.
+            PdfSplitOptions splitOptions = new PdfSplitOptions();
+            //Enable the Split tags property.
+            splitOptions.SplitTags = true;
+            //Split the document by ranges.
+            loadDocument.SplitByRanges(new int[,] { { 0, 1 }, { 1, 2 } }, splitOptions);
+
+            //Close the document.
+            loadDocument.Close(true);
+        }
+    }
+}
---
+++ Split PDFs/Remove-Unused-Resources-when-Splitting-PDF-Documents/.NET/Remove-Unused-Resources-when-Splitting-PDF-Documents/Program.cs
@@ -0,0 +1,31 @@
+﻿using Syncfusion.Pdf;
+using Syncfusion.Pdf.Parsing;
+
+namespace Remove_Unused_Resources_when_Splitting_PDF_Documents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Load the existing PDF file.
+            PdfLoadedDocument loadDocument = new PdfLoadedDocument(new FileStream("../../../Input.pdf", FileMode.Open));
+            //Subscribe to the document split event.
+            loadDocument.DocumentSplitEvent += LoadDocument_DocumentSplitEvent;
+            void LoadDocument_DocumentSplitEvent(object sender, PdfDocumentSplitEventArgs args)
+            {
+                //Save the resulting document.
+                FileStream outputStream = new FileStream(Guid.NewGuid().ToString() + ".pdf", FileMode.CreateNew);
+                args.PdfDocumentData.CopyTo(outputStream);
+                outputStream.Close();
+            }
+            //Create the split options object.
+            PdfSplitOptions splitOptions = new PdfSplitOptions();
+            //Enable the removal of unused resources property.
+            splitOptions.RemoveUnusedResources = true;
+            //Split the document by ranges.
+            loadDocument.SplitByRanges(new int[,] { { 0, 5 }, { 5, 10 } }, splitOptions);
+            //Close the document.
+            loadDocument.Close(true);
+        }
+    }
+}
