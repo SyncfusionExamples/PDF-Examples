bd4fd8b1128cc62be8eb6ffbdd1112d2446d2d7a
Srihariharan <srihariharan.g@syncfusion.com>
2024-01-09 11:27:45 +0530

    864873 :  Added Background color missing issue in HTML Header and Footer sample.

---
+++ HTML to PDF/Blink/HTML-Footer-Background-Colour/.NET/HTML-Footer-Background-Colour/Program.cs
@@ -0,0 +1,28 @@
+﻿//Initialize HTML to PDF converter.
+using Syncfusion.HtmlConverter;
+using Syncfusion.Pdf;
+using Syncfusion.Drawing;
+
+HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+//Initialize blink converter settings. 
+BlinkConverterSettings blinkConverterSettings = new BlinkConverterSettings();
+//Set the Blink viewport size.
+blinkConverterSettings.ViewPortSize = new Size(1280, 0);
+//Set the html margin-top value based on the html header height and margin-top value.
+blinkConverterSettings.Margin.Top = 70;
+//Set the html margin-bottom value based on the html footer height and margin-bottom value.
+blinkConverterSettings.Margin.Bottom = 40;
+//Set the custom HTML header to add at the top of each page.
+blinkConverterSettings.HtmlHeader = " <div style=\"background-color: blue; -webkit-print-color-adjust: exact; margin-left: 40px; font-size: 10px;\">HTML Header</div>";
+//Set the custom HTML footer to add at the bottom of each page.
+blinkConverterSettings.HtmlFooter = " <div style=\"background-color: blue; -webkit-print-color-adjust: exact;margin-left: 40px; font-size: 10px;\">HTML Footer</div>";
+//Assign Blink converter settings to the HTML converter.
+htmlConverter.ConverterSettings = blinkConverterSettings;
+//Convert the URL to a PDF document.
+PdfDocument document = htmlConverter.Convert("<div>Hello World</div>",string.Empty);
+
+//Create a filestream.
+FileStream fileStream = new FileStream("Output.pdf", FileMode.Create, FileAccess.ReadWrite);
+//Save and close a PDF document.
+document.Save(fileStream);
+document.Close(true);
