03233ae053a06ecfb76dd3db4ca960b667d8fc3e
Srihariharan <srihariharan.g@syncfusion.com>
2024-02-01 09:19:51 +0530

    868485:  Added missing files.

---
+++ Security/troubleshooting/.NET/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Syncfusion.Pdf.Parsing;
+using Syncfusion.Pdf.Security;
+using System.Security.Cryptography;
+
+FileStream fileStream = new FileStream("OwnerPasswordOnly.pdf", FileMode.Open, FileAccess.Read);
+PdfLoadedDocument loadedDocument = new PdfLoadedDocument(fileStream, "12345");
+PdfSecurity security = loadedDocument.Security;
+loadedDocument.FileStructure.IncrementalUpdate = false;
+loadedDocument.Security.Permissions = PdfPermissionsFlags.EditAnnotations | PdfPermissionsFlags.AssembleDocument;
+security.Algorithm = PdfEncryptionAlgorithm.AES;
+security.OwnerPassword = "";
+security.UserPassword = "";
+MemoryStream stream = new MemoryStream();
+loadedDocument.Save(stream);
+File.WriteAllBytes("output.pdf", stream.ToArray());
+loadedDocument.Close(true);
