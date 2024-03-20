861e8014468a8bd3313917aefbca3027deae7c5c
Srihariharan <srihariharan.g@syncfusion.com>
2024-03-20 11:28:09 +0530

    875092: HTML to PDF converter feature samples.

---
+++ HTML to PDF/Blink/Accessible_PDF_In_HTML_to_PDF/.NET/Accessible PDF/Program.cs
@@ -0,0 +1,18 @@
+﻿using Syncfusion.HtmlConverter;
+using Syncfusion.Pdf;
+
+//Initialize HTML to PDF converter.
+HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+//Initialize the BlinkConverterSettings.
+BlinkConverterSettings settings = new BlinkConverterSettings();
+//Set true to enable the accessibility tags in PDF generation.
+settings.EnableAccessibilityTags = true;
+//Assign the BlinkConverterSettings to the ConverterSettings property of HtmlToPdfConverter.
+htmlConverter.ConverterSettings = settings;
+//Convert URL to PDF.
+PdfDocument document = htmlConverter.Convert("file:///D:/PDF-Examples/HTML%20to%20PDF/Blink/Accessible_PDF_In_HTML_to_PDF/.NET/Accessible%20PDF/Input.html");
+//Save and close the PDF document.
+FileStream fileStream = new FileStream("HTML-to-PDF.pdf", FileMode.CreateNew, FileAccess.ReadWrite);
+//Save and close the PDF document 
+document.Save(fileStream);
+document.Close(true);
---
+++ HTML to PDF/Blink/Image_Background_In_HTML_to_PDF/.NET/Image_Background_In_HTML_to_PDF/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Syncfusion.Drawing;
+using Syncfusion.HtmlConverter;
+
+//Initialize HTML to PDF converter.
+HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+
+//Initialize the BlinkConverterSettings
+BlinkConverterSettings settings = new BlinkConverterSettings();
+
+//Set the Image Background color.
+settings.ImageBackgroundColor = Color.Transparent;
+
+//Assign the BlinkConverterSettings to the ConverterSettings property of HtmlToPdfConverter.
+htmlConverter.ConverterSettings = settings;
+
+//Convert HTML to Image
+Image image = htmlConverter.ConvertToImage("file:///D:/PDF-Examples/HTML%20to%20PDF/Blink/Image_Background_In_HTML_to_PDF/.NET/Image_Background_In_HTML_to_PDF/Input.html");
+
+//Save the Image.
+byte[] imageByte = image.ImageData;
+
+File.WriteAllBytes("Output.png", imageByte);
+
+
