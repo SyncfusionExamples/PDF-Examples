84d3a60f16713ac1e87690ee3460dacbed42ab1a
Srihariharan <srihariharan.g@syncfusion.com>
2024-04-04 10:42:39 +0530

    880343: Need to update the PDF section in the UG documentation.

---
+++ Watermark/Removing-watermark-annotation-in-PDF-document/.NET/Remove-watermark-annotation-in-the-PDF-document/Program.cs
@@ -0,0 +1,29 @@
+﻿using Syncfusion.Pdf.Parsing;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Interactive;
+
+//Load the PDF document
+FileStream docStream = new FileStream("../../../Input.pdf", FileMode.Open, FileAccess.Read);
+PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
+
+// Iterate through the annotations collection and remove PdfLoadedWatermark annotations
+foreach (PdfPageBase page in loadedDocument.Pages)
+{
+    for (int i = page.Annotations.Count - 1; i >= 0; i--)
+    {
+        // Check if the annotation is a PdfLoadedWatermarkAnnotation
+        if (page.Annotations[i] is PdfLoadedWatermarkAnnotation)
+        {
+            // Remove the PdfLoadedWatermarkAnnotation
+            page.Annotations.RemoveAt(i);
+        }
+    }
+}
+
+//Save the document.
+using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+{
+    loadedDocument.Save(outputFileStream);
+}
+//Close the document.
+loadedDocument.Close(true);
