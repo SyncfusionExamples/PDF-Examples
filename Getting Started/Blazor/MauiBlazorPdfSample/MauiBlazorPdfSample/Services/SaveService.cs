5ba7b671a8e1dccbc2ac3208987df154891694e8
Srihariharan <srihariharan.g@syncfusion.com>
2024-06-19 09:30:52 +0530

    886801: Added the sample for .NET MAUI Blazor application.

---
+++ Getting Started/Blazor/MauiBlazorPdfSample/MauiBlazorPdfSample/Services/SaveService.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace MauiBlazorPdfSample.Services
+{
+    public partial class SaveService
+    {
+        public partial void SaveAndView(string filename, string contentType, MemoryStream stream);
+    }
+}
