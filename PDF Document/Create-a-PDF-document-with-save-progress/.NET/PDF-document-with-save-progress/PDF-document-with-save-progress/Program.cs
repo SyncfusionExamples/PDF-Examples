3319566f4c17281ea91c75b28ace80e509bb956a
Srihariharan <srihariharan.g@syncfusion.com>
2024-07-04 12:33:05 +0530

    895490: PDF Save Progress sample.

---
+++ PDF Document/Create-a-PDF-document-with-save-progress/.NET/PDF-document-with-save-progress/PDF-document-with-save-progress/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf;
+using Syncfusion.Drawing;
+
+//Create a new PDF document.
+PdfDocument document = new PdfDocument();
+//Add new pages to the document.
+PdfPage page = document.Pages.Add();
+//Create font and font style.
+PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 12f, PdfFontStyle.Bold);
+//Draw text in the new page.
+page.Graphics.DrawString("Essential PDF", font, PdfBrushes.Black, new PointF(10, 10));
+document.SaveProgress += new PdfDocument.ProgressEventHandler(document_SaveProgress);
+
+
+//Create file stream.
+using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+{
+    //Save the PDF document to file stream.
+    document.Save(outputFileStream);
+}
+
+//Close the document.
+document.Close(true);
+// Event handler for PageAdded event
+void document_SaveProgress(object sender, ProgressEventArgs arguments)
+{
+    Console.WriteLine(String.Format("Current: {0}, Progress: {1}, Total {2}", arguments.Current, arguments.Progress, arguments.Total));
+}
