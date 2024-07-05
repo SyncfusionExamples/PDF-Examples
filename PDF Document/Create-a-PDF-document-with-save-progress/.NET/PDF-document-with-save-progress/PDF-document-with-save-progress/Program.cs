082a774a4f4eb04345c90651538a4af8f75b6acc
Srihariharan <srihariharan.g@syncfusion.com>
2024-07-05 10:12:14 +0530

    895490: Resolve the feedback.

--- PDF Document/Create-a-PDF-document-with-save-progress/.NET/PDF-document-with-save-progress/PDF-document-with-save-progress/Program.cs
+++ PDF Document/Create-a-PDF-document-with-save-progress/.NET/PDF-document-with-save-progress/PDF-document-with-save-progress/Program.cs
@@ -16,26 +16,29 @@ for (int i = 0; i < 10; i++)
     // Create PDF graphics for the page.
     PdfGraphics graphics = page.Graphics;
 
-    // Set the font.
+    // Set the font to Helvetica with size 20.
     PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 20);
 
     // Draw text on the page.
     graphics.DrawString($"This is page {i + 1}", font, PdfBrushes.Black, new PointF(0, 0));
 }
+
+// Subscribe to the SaveProgress event.
 document.SaveProgress += new PdfDocument.ProgressEventHandler(document_SaveProgress);
 
-
-//Create file stream.
+// Create a file stream to save the PDF document.
 using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
 {
-    //Save the PDF document to file stream.
+    // Save the PDF document to the file stream.
     document.Save(outputFileStream);
 }
 
-//Close the document.
+// Close the document.
 document.Close(true);
-// Event handler for PageAdded event
+
+// Event handler for the SaveProgress event.
 void document_SaveProgress(object sender, ProgressEventArgs arguments)
 {
-    Console.WriteLine(String.Format("Current: {0}, Progress: {1}, Total {2}", arguments.Current, arguments.Progress, arguments.Total));
+    // Output the current progress of the save operation.
+    Console.WriteLine(String.Format("Current: {0}, Progress: {1}, Total: {2}", arguments.Current, arguments.Progress, arguments.Total));
 }
