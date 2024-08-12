bddce4a65700f1d60ff61b37c5eb47ca2a4b58b4
Karmegam <103259299+karmegams02@users.noreply.github.com>
2024-08-12 17:06:23 +0530

    Resolved the vulnerability issue in the GitHub samples

--- Getting Started/ASP.NET MVC/Fill-form-in-an-existing-PDF-document/Fill-form-in-an-existing-PDF-document/App_Start/BundleConfig.cs
+++ Getting Started/ASP.NET MVC/Fill-form-in-an-existing-PDF-document/Fill-form-in-an-existing-PDF-document/App_Start/BundleConfig.cs
@@ -19,7 +19,7 @@ namespace Fill_form_in_an_existing_PDF_document
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
--- Getting Started/ASP.NET MVC/Merge-multiple-documents-from-stream/Merge-multiple-documents-from-stream/App_Start/BundleConfig.cs
+++ Getting Started/ASP.NET MVC/Merge-multiple-documents-from-stream/Merge-multiple-documents-from-stream/App_Start/BundleConfig.cs
@@ -19,7 +19,7 @@ namespace Merge_multiple_documents_from_stream
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
