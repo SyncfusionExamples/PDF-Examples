ca12fbf5ac60be8db5ad923bc5829b4b403a51e3
Srihariharan <srihariharan.g@syncfusion.com>
2024-08-27 10:09:42 +0530

    Playground_sample: Update PDF to PDF/A conversion progress sample.

--- PDF Conformance/Get-PDF-to-PDFA-conversion-progress/.NET/Get-PDF-to-PDFA-conversion-progress/Program.cs
+++ PDF Conformance/Get-PDF-to-PDFA-conversion-progress/.NET/Get-PDF-to-PDFA-conversion-progress/Program.cs
@@ -1,23 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+//using Syncfusion.Pdf;
+//using Syncfusion.Pdf.Parsing;
+
+////Get stream from an existing PDF document. 
+//FileStream docStream = new FileStream(Path.GetFullPath(@"Data/Input.pdf"), FileMode.Open, FileAccess.Read);
+
+////Load an existing PDF document. 
+//PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
+
+////Set the conformance for PDF/A-1b conversion.
+//loadedDocument.Conformance = PdfConformanceLevel.Pdf_A1B;
+
+//loadedDocument.PdfAConversionProgress += pdfAConversion_TrackProgress;
+
+////Event handler for Track PDF to PDF/A conversion process
+//void pdfAConversion_TrackProgress(object sender, PdfAConversionProgressEventArgs arguments)
+//{
+//    Console.WriteLine(String.Format("PDF to PDF/A conversion process " + arguments.ProgressValue + "% completed"));
+//    Console.ReadLine();
+//}
+
 using Syncfusion.Pdf;
 using Syncfusion.Pdf.Parsing;
 
-//Get stream from an existing PDF document. 
+//Get stream from an existing PDF document.
 FileStream docStream = new FileStream(Path.GetFullPath(@"Data/Input.pdf"), FileMode.Open, FileAccess.Read);
 
-//Load an existing PDF document. 
+//Load an existing PDF document.
 PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
 
-//Set the conformance for PDF/A-1b conversion.
-loadedDocument.Conformance = PdfConformanceLevel.Pdf_A1B;
+//Subscribe to the PdfAConversionProgress event to track the PDF to PDF/A conversion process
+loadedDocument.PdfAConversionProgress += new PdfLoadedDocument.PdfAConversionProgressEventHandler(pdfAConversion_TrackProgress);
 
-loadedDocument.PdfAConversionProgress += pdfAConversion_TrackProgress;
+loadedDocument.ConvertToPDFA(PdfConformanceLevel.Pdf_A1B);
+
+//Save the document
+FileStream outputStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.Write);
+loadedDocument.Save(outputStream);
+
+//Close the document
+loadedDocument.Close(true);
+
 
 //Event handler for Track PDF to PDF/A conversion process
 void pdfAConversion_TrackProgress(object sender, PdfAConversionProgressEventArgs arguments)
 {
     Console.WriteLine(String.Format("PDF to PDF/A conversion process " + arguments.ProgressValue + "% completed"));
-    Console.ReadLine();
 }
-
