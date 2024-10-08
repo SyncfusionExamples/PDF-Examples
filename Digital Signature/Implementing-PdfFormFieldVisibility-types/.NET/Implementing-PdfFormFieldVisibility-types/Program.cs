b7c14681c2876a6656065f536e9dcfd43e0e86b4
Sameerkhan <sameerkhan.n@syncfusion.com>
2024-10-08 17:34:26 +0530

    913761 Added form fields visibility sample project.

---
+++ Digital Signature/Implementing-PdfFormFieldVisibility-types/.NET/Implementing-PdfFormFieldVisibility-types/Program.cs
@@ -0,0 +1,27 @@
+﻿using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf;
+using Syncfusion.Drawing;
+
+PdfDocument document = new PdfDocument();
+//Creates a new page and adds it as the last page of the document
+PdfPage page = document.Pages.Add();
+PdfFont font = new PdfStandardFont(PdfFontFamily.Courier, 12f);
+//Create a text box
+PdfTextBoxField firstNameTextBox = new PdfTextBoxField(page, "firstNameTextBox");
+firstNameTextBox.MaxLength = 8;
+firstNameTextBox.Bounds = new RectangleF(100, 20, 200, 20);
+firstNameTextBox.Font = font;
+firstNameTextBox.Text = "Text Box";
+//Set the visibility.
+firstNameTextBox.Visibility = PdfFormFieldVisibility.Visible;
+page.Graphics.DrawString("First Name", font, PdfBrushes.Black, 10, 55);
+//Add the textbox in document
+document.Form.Fields.Add(firstNameTextBox);
+//Save the document.
+using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+{
+    document.Save(outputFileStream);
+}
+//Close the document
+document.Close(true);
