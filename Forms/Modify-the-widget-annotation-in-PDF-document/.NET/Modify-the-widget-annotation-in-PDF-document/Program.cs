4c781d46114389c7f11da10916ae4ef8ae74a3cb
Sameerkhan <sameerkhan.n@syncfusion.com>
2024-11-12 13:24:57 +0530

    920569 Modified given feedback.

--- Forms/Modify-the-widget-annotation-in-PDF-document/.NET/Modify-the-widget-annotation-in-PDF-document/Program.cs
+++ Forms/Modify-the-widget-annotation-in-PDF-document/.NET/Modify-the-widget-annotation-in-PDF-document/Program.cs
@@ -8,24 +8,19 @@ PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
 // Loop through each page in the loaded PDF document.
 foreach (PdfLoadedPage page in loadedDocument.Pages)
 {
-    // Access the form within the loaded document.
-    PdfLoadedForm form = loadedDocument.Form;
-    // Check if the forms are null.
-    if (form == null)
+    // Loop through each annotation on the current page.
+    foreach (PdfLoadedAnnotation annot in page.Annotations)
     {
-        // Loop through each annotation on the current page.
-        foreach (PdfLoadedAnnotation annot in page.Annotations)
+        // Check if the annotation is a widget annotation.
+        if (annot is PdfLoadedWidgetAnnotation)
         {
-            // Check if the annotation is a widget annotation.
-            if (annot is PdfLoadedWidgetAnnotation)
-            {
-                // Cast the annotation to a PdfLoadedWidgetAnnotation type.
-                PdfLoadedWidgetAnnotation widget = annot as PdfLoadedWidgetAnnotation;
-                // Set the widget's text value to "true".
-                widget.Text = "true";
-            }
+            // Cast the annotation to a PdfLoadedWidgetAnnotation type.
+            PdfLoadedWidgetAnnotation widget = annot as PdfLoadedWidgetAnnotation;
+            // Set the widget's text value to "true".
+            widget.Text = "true";
         }
     }
+
 }
 //Create file stream.
 using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
