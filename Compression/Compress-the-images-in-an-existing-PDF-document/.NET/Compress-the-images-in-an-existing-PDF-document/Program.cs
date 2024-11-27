048d323ed609aba4bcc10e54b54a5480eb38b07a
Sameerkhan <sameerkhan.n@syncfusion.com>
2024-11-27 10:12:58 +0530

    921490_ft1 Added using statement in compression.

--- Compression/Compress-the-images-in-an-existing-PDF-document/.NET/Compress-the-images-in-an-existing-PDF-document/Program.cs
+++ Compression/Compress-the-images-in-an-existing-PDF-document/.NET/Compress-the-images-in-an-existing-PDF-document/Program.cs
@@ -4,30 +4,27 @@ using Syncfusion.Pdf;
 using Syncfusion.Pdf.Parsing;
 
 //Get stream from an existing PDF document. 
-FileStream docStream = new FileStream(Path.GetFullPath(@"Data/imageDoc.pdf"), FileMode.Open, FileAccess.Read);
-
-//Load the existing PDF document.
-PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
-
-//Create a new compression option.
-PdfCompressionOptions options = new PdfCompressionOptions();
-
-//Enable the compress image.
-options.CompressImages = true;
-
-//Set the image quality.
-options.ImageQuality = 50;
-
-//Assign the compression option to the document
-loadedDocument.Compress(options);
-
-//Create file stream.
-using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+using (FileStream docStream = new FileStream(Path.GetFullPath(@"Data/imageDoc.pdf"), FileMode.Open, FileAccess.Read))
 {
-    //Save the PDF document to file stream.
-    loadedDocument.Save(outputFileStream);
-}
 
-//Close the document.
-loadedDocument.Close(true);
+    //Load the existing PDF document.
+    PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
+    //Create a new compression option.
+    PdfCompressionOptions options = new PdfCompressionOptions();
+    //Enable the compress image.
+    options.CompressImages = true;
+    //Set the image quality.
+    options.ImageQuality = 50;
+    //Assign the compression option to the document
+    loadedDocument.Compress(options);
+
+    //Create file stream.
+    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+    {
+        //Save the PDF document to file stream.
+        loadedDocument.Save(outputFileStream);
+    }
+    //Close the document.
+    loadedDocument.Close(true);
+}
 
