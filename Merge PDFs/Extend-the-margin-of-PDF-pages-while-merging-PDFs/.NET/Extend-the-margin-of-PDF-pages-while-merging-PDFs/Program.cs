30beb6afb012734da3e65d7ac7ba5dba4ca59d15
Sameerkhan <sameerkhan.n@syncfusion.com>
2024-11-27 09:43:52 +0530

    921490_ft1 Resolved the failures.

--- Merge PDFs/Extend-the-margin-of-PDF-pages-while-merging-PDFs/.NET/Extend-the-margin-of-PDF-pages-while-merging-PDFs/Program.cs
+++ Merge PDFs/Extend-the-margin-of-PDF-pages-while-merging-PDFs/.NET/Extend-the-margin-of-PDF-pages-while-merging-PDFs/Program.cs
@@ -4,33 +4,33 @@ using Syncfusion.Pdf;
 using Syncfusion.Pdf.Graphics;
 
 //Create a new PDF document 
-using (PdfDocument outputDocument = new PdfDocument()) 
-{ 
+using (PdfDocument outputDocument = new PdfDocument())
+{
     //Create a new instance for the document margin 
-    PdfMargins documentMargins = new PdfMargins(); 
+    PdfMargins documentMargins = new PdfMargins();
     //Set the margin to 50 points on all sides  
-    documentMargins.All = 50; 
+    documentMargins.All = 50;
     //Set the document margins 
-    outputDocument.PageSettings.Margins = documentMargins; 
+    outputDocument.PageSettings.Margins = documentMargins;
     //Load the first PDF document 
-    using (FileStream firstPDFStream = new FileStream(Path.GetFullPath(@"Data/File1.pdf"), FileMode.Open, FileAccess.Read)) 
-    { 
+    using (FileStream firstPDFStream = new FileStream(Path.GetFullPath(@"Data/File1.pdf"), FileMode.Open, FileAccess.Read))
+    {
         //Load the second PDF document 
-        using (FileStream secondPDFStream = new FileStream(Path.GetFullPath(@"Data/File2.pdf"), FileMode.Open, FileAccess.Read)) 
-        { 
+        using (FileStream secondPDFStream = new FileStream(Path.GetFullPath(@"Data/File2.pdf"), FileMode.Open, FileAccess.Read))
+        {
             //Create a list of streams to merge 
-            Stream[] streams = { firstPDFStream, secondPDFStream }; 
+            Stream[] streams = { firstPDFStream, secondPDFStream };
             //Create a merge options object 
-            PdfMergeOptions mergeOptions = new PdfMergeOptions(); 
+            PdfMergeOptions mergeOptions = new PdfMergeOptions();
             //Enable the extend margin option 
-            mergeOptions.ExtendMargin = true; 
+            mergeOptions.ExtendMargin = true;
             //Merge the PDF documents  
-            PdfDocumentBase.Merge(outputDocument, mergeOptions, streams); 
+            PdfDocumentBase.Merge(outputDocument, mergeOptions, streams);
             //Save the document to a memory stream 
             using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
-{ 
-                outputDocument.Save(outputFileStream); 
-            } 
-        } 
-    } 
+            {
+                outputDocument.Save(outputFileStream);
+            }
+        }
+    }
 }
--- Redaction/Removing-sensitive-content-from-the-PDF-document/.NET/Removing-sensitive-content-from-the-PDF-document/Program.cs
+++ Redaction/Removing-sensitive-content-from-the-PDF-document/.NET/Removing-sensitive-content-from-the-PDF-document/Program.cs
@@ -9,15 +9,20 @@ using Syncfusion.Pdf.Redaction;
 using (FileStream docStream = new FileStream(Path.GetFullPath(@"Data/Input.pdf"), FileMode.Open, FileAccess.Read))
 {
     //Load the PDF document from stream
-    using (PdfLoadedDocument loadedDocument = new PdfLoadedDocument(inputStream))
+    using (PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream))
     {
         PdfLoadedPage loadedPage = loadedDocument.Pages[0] as PdfLoadedPage;
         //Create a PDF redaction for the page
-        PdfRedaction redaction = new PdfRedaction(new RectangleF(340, 120, 140, 20));
-        //Add redaction to the first page
-        loadedPage.Redactions.Add(redaction);
+        PdfRedaction redaction = new PdfRedaction(new RectangleF(340, 120, 140, 20), Color.Black);
+        //Add a redaction object into the redaction collection of loaded page.
+        loadedPage.AddRedaction(redaction);
+        //Redact the contents from the PDF document.
+        loadedDocument.Redact();
+
         //Save the redacted PDF document to the memory stream
-        MemoryStream stream = new MemoryStream();
-        loadedDocument.Save(stream);
+        using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+        {
+            loadedDocument.Save(outputFileStream);
+        }
     }
 }
