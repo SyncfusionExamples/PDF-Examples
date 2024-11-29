71fd4680ca6ac07553ae1b901c13ced01ec12e52
Sameerkhan <sameerkhan.n@syncfusion.com>
2024-11-29 11:02:41 +0530

    921490_ft1 Resolved code changes.

--- Images/Convert_Image_to_PDF/.NET/Convert_Image_to_PDF/Program.cs
+++ Images/Convert_Image_to_PDF/.NET/Convert_Image_to_PDF/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Syncfusion.Pdf;
 
 // Create an instance of the ImageToPdfConverter class 
-var imageToPdfConverter = new ImageToPdfConverter();
+ImageToPdfConverter imageToPdfConverter = new ImageToPdfConverter();
 
 // Set the page size for the PDF 
 imageToPdfConverter.PageSize = PdfPageSize.A4;
@@ -10,13 +10,13 @@ imageToPdfConverter.PageSize = PdfPageSize.A4;
 imageToPdfConverter.ImagePosition = PdfImagePosition.TopLeftCornerOfPage;
 
 // Create a file stream to read the image file 
-using (var imageStream = new FileStream(Path.GetFullPath(@"../../../Data/Autumn Leaves.jpg"), FileMode.Open, FileAccess.Read))
+using (FileStream imageStream = new FileStream(Path.GetFullPath(@"Data/Autumn Leaves.jpg"), FileMode.Open, FileAccess.Read))
 {
     // Convert the image to a PDF document using the ImageToPdfConverter 
     PdfDocument pdfDocument = imageToPdfConverter.Convert(imageStream);
 
     //Create file stream.
-    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
     {
         //Save the PDF document to file stream.
         pdfDocument.Save(outputFileStream);
