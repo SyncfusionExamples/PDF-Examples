7e0ce012ff079b1125255d9584c8a39f2b1127db
Sameerkhan <sameerkhan.n@syncfusion.com>
2024-11-29 10:59:45 +0530

    921490_ft1 Added image to PDF sample code

---
+++ Images/Convert_Image_to_PDF/.NET/Convert_Image_to_PDF/Program.cs
@@ -0,0 +1,27 @@
+﻿using Syncfusion.Pdf;
+
+// Create an instance of the ImageToPdfConverter class 
+var imageToPdfConverter = new ImageToPdfConverter();
+
+// Set the page size for the PDF 
+imageToPdfConverter.PageSize = PdfPageSize.A4;
+
+// Set the position of the image in the PDF 
+imageToPdfConverter.ImagePosition = PdfImagePosition.TopLeftCornerOfPage;
+
+// Create a file stream to read the image file 
+using (var imageStream = new FileStream(Path.GetFullPath(@"../../../Data/Autumn Leaves.jpg"), FileMode.Open, FileAccess.Read))
+{
+    // Convert the image to a PDF document using the ImageToPdfConverter 
+    PdfDocument pdfDocument = imageToPdfConverter.Convert(imageStream);
+
+    //Create file stream.
+    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+    {
+        //Save the PDF document to file stream.
+        pdfDocument.Save(outputFileStream);
+    }
+
+    // Close the document 
+    pdfDocument.Close(true);
+}
