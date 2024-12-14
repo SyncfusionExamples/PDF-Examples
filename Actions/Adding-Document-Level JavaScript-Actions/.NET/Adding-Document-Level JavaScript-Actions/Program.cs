c0e933a538248b3ae03b6d1929125a42df3d6ab9
Sameerkhan <sameerkhan.n@syncfusion.com>
2024-12-14 16:03:45 +0530

    927950 Added new feature samples.

---
+++ Actions/Adding-Document-Level JavaScript-Actions/.NET/Adding-Document-Level JavaScript-Actions/Program.cs
@@ -0,0 +1,25 @@
+﻿using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf;
+
+// Create a new PDF document instance.
+PdfDocument document = new PdfDocument();
+
+// Retrieve the JavaScript collection from the document.
+PdfDocumentJavaScriptCollection javaScriptCollection = document.DocumentJavaScripts;
+
+// Define a new JavaScript action that displays an alert with the message "Hello World!!!".
+PdfJavaScriptAction javaScriptAction = new PdfJavaScriptAction("app.alert(\"Hello World!!!\")");
+
+// Set the name of the JavaScript action.
+javaScriptAction.Name = "Test";
+
+// Add the JavaScript action to the document's JavaScript collection.
+javaScriptCollection.Add(javaScriptAction);
+
+// Save the PDF document using a file stream.
+using (FileStream fileStream = new FileStream(Path.GetFullPath(@"Output\Output.pdf"), FileMode.Create, FileAccess.Write))
+{
+    document.Save(fileStream);
+}
+// Close the document to release resources.
+document.Close(true);
---
+++ Actions/Retrieve-Document-Level-JavaScript-Actions/.NET/Retrieve-Document-Level-JavaScript-Actions/Program.cs
@@ -0,0 +1,21 @@
+﻿using Syncfusion.Pdf;
+using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf.Parsing;
+
+//Load an existing PDF document using a file stream
+using (FileStream fileStream = new FileStream(Path.GetFullPath(@"Data\Input.pdf"), FileMode.Open, FileAccess.Read))
+using (PdfLoadedDocument document = new PdfLoadedDocument(fileStream))
+{
+    //Retrieve the JavaScript collection from the loaded document
+    PdfDocumentJavaScriptCollection javaScriptCollection = document.DocumentJavaScripts;
+
+    //Iterate through the JavaScript actions in the collection
+    foreach (PdfJavaScriptAction action in javaScriptCollection)
+    {
+        //Display the name of the JavaScript action
+        Console.WriteLine($"Action Name: {action.Name}");
+
+        //Display the JavaScript code associated with the action
+        Console.WriteLine($"JavaScript Code: {action.JavaScript}");
+    }
+}
