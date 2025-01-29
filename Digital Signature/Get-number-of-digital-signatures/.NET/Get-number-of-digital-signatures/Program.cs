dd7b509fabb4c52a45451fd4ec990b6aa3298d18
Sameerkhan <sameerkhan.n@syncfusion.com>
2025-01-29 12:49:24 +0530

    935299 Addressed the given feedback.

--- Digital Signature/Get-number-of-digital-signatures/.NET/Get-number-of-digital-signatures/Program.cs
+++ Digital Signature/Get-number-of-digital-signatures/.NET/Get-number-of-digital-signatures/Program.cs
@@ -1,32 +1,35 @@
 ﻿using Syncfusion.Pdf.Parsing;
 
 // Initialize a counter to count the number of signature fields in the PDF document
-int count = 0; 
+int signedFieldsCount = 0; 
+int unSignedFieldsCount = 0;
 
 // Open the PDF document as a file stream
 using (FileStream docStream = new FileStream(Path.GetFullPath(@"Data/Input.pdf"), FileMode.Open, FileAccess.Read))
 {
-    // Load the PDF document from the file stream
-    using (PdfLoadedDocument doc = new PdfLoadedDocument(docStream))
-    {
-        // Access the form inside the loaded PDF document
-        PdfLoadedForm form = doc.Form;
+   // Load the PDF document from the file stream
+   using (PdfLoadedDocument doc = new PdfLoadedDocument(docStream))
+   {
+       // Access the form inside the loaded PDF document
+       PdfLoadedForm form = doc.Form;        
 
-        // Retrieve the collection of form fields present in the form
-        PdfLoadedFormFieldCollection fieldCollection = form.Fields as PdfLoadedFormFieldCollection;
+       // Iterate through each field in the form field collection
+       foreach (PdfLoadedField field in form.Fields)
+       {
+           PdfLoadedSignatureField sigField = field as PdfLoadedSignatureField;
+           // Check whether the signature field is signed or not
+           if (sigField != null && sigField.IsSigned)
+           {               
+               signedFieldsCount ++; 
+           }
+           else
+           {
+              unSignedFieldsCount ++;
+           }
+       }
 
-        // Iterate through each field in the form field collection
-        foreach (PdfLoadedField field in fieldCollection)
-        {
-            // Check if the field is a signature field
-            if (field is PdfLoadedSignatureField)
-            {
-                // Increment the counter if a signature field is found
-                count++; 
-            }
-        }
-
-        // Display the total number of signature fields found in the PDF document
-        Console.WriteLine("The number of signature fields in the PDF document: " + count);
-    }
+       // Display the total number of signature fields found in the PDF document
+       Console.WriteLine("The number of signed signature fields in the PDF document: " + signedFieldsCount);
+       Console.WriteLine("The number of unSigned signature fields in the PDF document: " + unSignedFieldsCount);
+   }
 }
