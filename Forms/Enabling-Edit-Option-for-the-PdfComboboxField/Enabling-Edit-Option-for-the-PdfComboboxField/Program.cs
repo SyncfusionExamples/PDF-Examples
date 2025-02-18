3f990da4b9e3879d71132cf4f2a97ec948f82754
Irfana Jaffer Sadhik <irfana.jaffersadhik@syncfusion.com>
2025-02-18 18:34:06 +0530

    modified the sample

--- Forms/Enabling-Edit-Option-for-the-PdfComboboxField/Enabling-Edit-Option-for-the-PdfComboboxField/Program.cs
+++ Forms/Enabling-Edit-Option-for-the-PdfComboboxField/Enabling-Edit-Option-for-the-PdfComboboxField/Program.cs
@@ -1,38 +1,36 @@
-﻿//Create a new PDf document
-
-using Syncfusion.Pdf;
+﻿//Create a new PDF document
 using Syncfusion.Pdf.Graphics;
 using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf;
 using Syncfusion.Drawing;
 
 PdfDocument document = new PdfDocument();
 
-//Creates a new page and adds it as the last page of the document
-
+//Add page
 PdfPage page = document.Pages.Add();
 
+//Create a PDF standard font
 PdfFont font = new PdfStandardFont(PdfFontFamily.Courier, 12f);
 
 //Create a combo box
 
 PdfComboBoxField positionComboBox = new PdfComboBoxField(page, "positionComboBox");
 
-positionComboBox.Editable = true;
-
+//Set the combo box bounds
 positionComboBox.Bounds = new RectangleF(100, 115, 200, 20);
 
+//Set the font
 positionComboBox.Font = font;
 
+//Enable editing option
 positionComboBox.Editable = true;
 
 //Add it to document
-
 document.Form.Fields.Add(positionComboBox);
 
-using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
 {
     //Save the PDF document to file stream.
     document.Save(outputFileStream);
 }
 document.Close(true);
-
