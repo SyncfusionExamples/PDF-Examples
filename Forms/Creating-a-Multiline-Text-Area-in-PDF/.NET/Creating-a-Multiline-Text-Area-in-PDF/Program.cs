2544ba66795f5457d2f1be00f348d20a6f7ccb68
sameerkhan001 <sameerkhan.n@syncfusion.com>
2025-03-07 11:15:16 +0530

    42167 Added sample code for created a multiline text area in PDF documents

---
+++ Forms/Creating-a-Multiline-Text-Area-in-PDF/.NET/Creating-a-Multiline-Text-Area-in-PDF/Program.cs
@@ -0,0 +1,34 @@
+﻿using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf;
+using Syncfusion.Drawing;
+
+// Create a new PDF document
+PdfDocument document = new PdfDocument();
+// Creates a new page and adds it as the last page of the document
+PdfPage page = document.Pages.Add();
+// Create a standard font to be used in the text field
+PdfFont font = new PdfStandardFont(PdfFontFamily.Courier, 12f);
+// Create a text box field and add it to the page with a name
+PdfTextBoxField textBoxField = new PdfTextBoxField(page, "FirstName");
+// Set the bounds (position and size) of the text box field
+textBoxField.Bounds = new RectangleF(0, 0, 200, 50);
+// Set the tooltip that will appear when the user hovers over the text box
+textBoxField.ToolTip = "First Name";
+
+// Enable multiline functionality for the text box (allows multiple lines of text)
+textBoxField.Multiline = true;
+
+// Set the initial text inside the text box field
+textBoxField.Text = "Essential PDF allows you to create and manage the form (AcroForm) in PDF document by using PdfForm class. The PdfFormFieldCollection class represents the entire field collection of the form.";
+// Add the text box field to the form field collection in the document
+document.Form.Fields.Add(textBoxField);
+
+// Create a FileStream to save the document
+using (FileStream stream = new FileStream(Path.GetFullPath(@"Output/Sample.pdf"), FileMode.Create, FileAccess.Write))
+{
+    // Save the PDF document to the memory stream
+    document.Save(stream);
+}
+// Close the document
+document.Close(true);
