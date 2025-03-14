6f611e67de35df5cba0ee918ceb74a4a5d5c4019
sameerkhan001 <sameerkhan.n@syncfusion.com>
2025-03-14 11:20:34 +0530

    937636 Modified the code changes.

---
+++ Digital Signature/Verify-digital-sign-in-existing-PDF/.NET/Verify-digital-sign-in-existing-PDF/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Reflection.Metadata;
+using Syncfusion.Pdf.Parsing;
+
+
+// Open the signed PDF file for reading
+using (FileStream inputFileStream = new FileStream(Path.GetFullPath(@"Data/Input.pdf"), FileMode.Open, FileAccess.Read))
+{
+    // Load the PDF document
+    PdfLoadedDocument loadedDocument = new PdfLoadedDocument(inputFileStream);
+
+    // Check if the document contains a form with fields
+    if (loadedDocument.Form == null || loadedDocument.Form.Fields.Count == 0)
+    {
+        Console.WriteLine("No signature fields found in the document.");
+    }
+    else
+    {
+        // Iterate through all fields in the form
+        foreach (PdfLoadedField field in loadedDocument.Form.Fields)
+        {
+            // Check if the field is a signature field
+            PdfLoadedSignatureField signatureField = field as PdfLoadedSignatureField;
+            if (signatureField != null)
+            {
+                // Determine whether the signature field is signed or not
+                string status = signatureField.IsSigned ? "Signed" : "UnSigned";
+
+                // Output the result for each signature field
+                Console.WriteLine("Signature Field " + signatureField.Name + " is: " + status);
+            }
+        }
+    }
+    //Close the document
+    loadedDocument.Close(true);
+}
