cef4df5296df7b3290353f495b81af04151c77c3
sameerkhan001 <sameerkhan.n@syncfusion.com>
2025-04-08 12:28:34 +0530

    951720 Added sample code for line breaks in OCR.

---
+++ OCR/.NET/Adding-Line-Breaks-Using-OCR/.NET/Adding-Line-Breaks-Using-OCR/Program.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.OCRProcessor;
+using Syncfusion.Pdf.Parsing;
+
+// Initialize the OCR processor
+using (OCRProcessor processor = new OCRProcessor())
+{
+    // Load the existing PDF document
+    using (FileStream stream = new FileStream(Path.GetFullPath(@"Data/Input.pdf"), FileMode.Open))
+    {
+        PdfLoadedDocument pdfLoadedDocument = new PdfLoadedDocument(stream);
+
+        // Set OCR language to process
+        processor.Settings.Language = Languages.English;
+
+        processor.Settings.PageSegment = PageSegMode.SparseTextOsd;
+
+        // Process OCR by providing the PDF document
+        processor.PerformOCR(pdfLoadedDocument, processor.TessDataPath, out OCRLayoutResult layoutResult);
+        string ocrText = string.Join("\n", layoutResult.Pages[0].Lines.Select(line => line.Text));
+
+
+        //Create file stream.
+        using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+        {
+            //Save the PDF document to file stream.
+            pdfLoadedDocument.Save(outputFileStream);
+        }
+        //Close the document.
+        pdfLoadedDocument.Close(true);
+        File.WriteAllText(Path.GetFullPath(@"Output/Output.txt"), ocrText);
+    }
+}
