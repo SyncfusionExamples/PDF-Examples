4fbd3fb9c2f2f7e96178b6a1f8d0b92635df9832
sameerkhan001 <sameerkhan.n@syncfusion.com>
2025-06-25 12:09:03 +0530

    957177: Added sample code for SVG to image using HTMLConverter.

---
+++ HTML to PDF/Blink/Converting_SVG_to_Image_using_HtmlConverter/.NET/Converting_SVG_to_Image_using_HtmlConverter/Program.cs
@@ -0,0 +1,9 @@
+﻿using Syncfusion.Drawing;
+using Syncfusion.HtmlConverter;
+
+HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+string svg = File.ReadAllText(Path.GetFullPath(@"Data/Input.svg"));
+Image images = htmlConverter.ConvertToImage(svg, "");
+byte[] imageByte = images.ImageData;
+//Save the image
+File.WriteAllBytes(Path.GetFullPath(@"Output/Output.jpg"), imageByte);
