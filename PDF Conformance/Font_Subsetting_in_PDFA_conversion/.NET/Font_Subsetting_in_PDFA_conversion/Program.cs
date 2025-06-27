1bdf8cf14037bf0730a3eba233c42d45ca5c4da5
sameerkhan001 <sameerkhan.n@syncfusion.com>
2025-06-27 12:57:04 +0530

    966446: Added new features sample project.

---
+++ PDF Conformance/Font_Subsetting_in_PDFA_conversion/.NET/Font_Subsetting_in_PDFA_conversion/Program.cs
@@ -0,0 +1,78 @@
+﻿using Syncfusion.Pdf.Parsing;
+using Syncfusion.Pdf;
+using System.Reflection.Metadata;
+using Syncfusion.Drawing;
+using Syncfusion.Pdf.Graphics;
+using SkiaSharp;
+
+//Get stream from an existing PDF document. 
+FileStream docStream = new FileStream(Path.GetFullPath(@"Data/Input.pdf"), FileMode.Open, FileAccess.Read);
+PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
+
+//Sample level font event handling  
+loadedDocument.SubstituteFont += LoadedDocument_SubstituteFont;
+
+//Create conformance options 
+PdfConformanceOptions options = new PdfConformanceOptions();
+//Set the conformance level 
+options.ConformanceLevel = PdfConformanceLevel.Pdf_A1B;
+
+//Embed fonts as subsets  
+options.SubsetFonts = true;
+
+// Convert to PDF/A conformance 
+loadedDocument.ConvertToPDFA(options);
+
+//Create file stream.
+using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+{
+    //Save the PDF document to file stream.
+    loadedDocument.Save(outputFileStream);
+}
+
+//Close the document.
+loadedDocument.Close(true);
+docStream.Close();
+
+static void LoadedDocument_SubstituteFont(object sender, PdfFontEventArgs args)
+{
+    //get the font name 
+    string fontName = args.FontName.Split(',')[0];
+
+    //get the font style 
+    PdfFontStyle fontStyle = args.FontStyle;
+    SKFontStyle sKFontStyle = SKFontStyle.Normal;
+
+    if (fontStyle != PdfFontStyle.Regular)
+    {
+        if (fontStyle == PdfFontStyle.Bold)
+        {
+            sKFontStyle = SKFontStyle.Bold;
+        }
+        else if (fontStyle == PdfFontStyle.Italic)
+        {
+            sKFontStyle = SKFontStyle.Italic;
+        }
+        else if (fontStyle == (PdfFontStyle.Italic | PdfFontStyle.Bold))
+        {
+            sKFontStyle = SKFontStyle.BoldItalic;
+        }
+    }
+
+    SKTypeface typeface = SKTypeface.FromFamilyName(fontName, sKFontStyle);
+    SKStreamAsset typeFaceStream = typeface.OpenStream();
+    MemoryStream memoryStream = null;
+    if (typeFaceStream != null && typeFaceStream.Length > 0)
+    {
+        //Create the fontData from the type face stream.	  
+        byte[] fontData = new byte[typeFaceStream.Length];
+        typeFaceStream.Read(fontData, typeFaceStream.Length);
+        typeFaceStream.Dispose();
+
+        //Create the new memory stream from the font data.	  
+        memoryStream = new MemoryStream(fontData);
+    }
+
+    //set the font stream to the event args.	 
+    args.FontStream = memoryStream;
+}
---
+++ Redaction/Redaction-fill-color-customization/.NET/Redaction-fill-color-customization/Program.cs
@@ -0,0 +1,45 @@
+﻿using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf;
+using Syncfusion.Drawing;
+
+//Create a new PDF document. 
+PdfDocument document = new PdfDocument();
+//Add a page to the document. 
+PdfPage page = document.Pages.Add();
+
+//Create a new Redaction annotation 
+PdfRedactionAnnotation annot = new PdfRedactionAnnotation();
+//Assign the Bounds Value 
+annot.Bounds = new Rectangle(100, 120, 100, 100);
+//Assign the InnerColor 
+annot.InnerColor = Color.Black;
+//Assign the BorderColor 
+annot.BorderColor = Color.Yellow;
+
+//Assign the AppearanceFillColor 
+annot.AppearanceFillColor = Color.BlueViolet;
+
+//Assign tbe TextColor 
+annot.TextColor = Color.Blue;
+//Assign the font 
+annot.Font = new PdfStandardFont(PdfFontFamily.Helvetica, 10);
+//Assign the OverlayText 
+annot.OverlayText = "REDACTION";
+//Assign the TextAlignment 
+annot.TextAlignment = PdfTextAlignment.Right;
+//Assign the RepeatText 
+annot.RepeatText = true;
+annot.SetAppearance(true);
+
+//Add the annotation to the page 
+page.Annotations.Add(annot);
+
+//Create file stream.
+using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+{
+    //Save the PDF document to file stream.
+    document.Save(outputFileStream);
+}
+//Close the document.
+document.Close(true);
