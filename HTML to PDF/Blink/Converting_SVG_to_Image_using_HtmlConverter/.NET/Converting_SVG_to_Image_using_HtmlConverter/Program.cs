f549d4e790226937bd989840ed436a312e8ff77d
sameerkhan001 <sameerkhan.n@syncfusion.com>
2025-06-30 10:42:06 +0530

    957177: Resolved the given feedback.

--- HTML to PDF/Blink/Converting_SVG_to_Image_using_HtmlConverter/.NET/Converting_SVG_to_Image_using_HtmlConverter/Program.cs
+++ HTML to PDF/Blink/Converting_SVG_to_Image_using_HtmlConverter/.NET/Converting_SVG_to_Image_using_HtmlConverter/Program.cs
@@ -3,7 +3,11 @@ using Syncfusion.HtmlConverter;
 
 HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
 string svg = File.ReadAllText(Path.GetFullPath(@"Data/Input.svg"));
-Image images = htmlConverter.ConvertToImage(svg, "");
-byte[] imageByte = images.ImageData;
-//Save the image
-File.WriteAllBytes(Path.GetFullPath(@"Output/Output.jpg"), imageByte);
+// Convert the SVG content to an image using the converter
+Image image = htmlConverter.ConvertToImage(svg, "");
+
+// Extract the image data as a byte array
+byte[] imageBytes = image.ImageData;
+
+// Save the image data to a file in JPEG format
+File.WriteAllBytes(Path.GetFullPath(@"Output/Output.jpg"), imageBytes);
