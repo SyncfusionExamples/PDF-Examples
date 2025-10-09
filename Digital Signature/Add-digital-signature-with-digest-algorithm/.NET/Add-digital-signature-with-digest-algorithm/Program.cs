b06d5257072014035163c7337bc93624b1e82eb4
sameerkhan001 <sameerkhan.n@syncfusion.com>
2025-10-09 18:46:08 +0530

    986143: Updated the SignedName details also in the Signature information in Digital Signature

--- Digital Signature/Add-digital-signature-with-digest-algorithm/.NET/Add-digital-signature-with-digest-algorithm/Program.cs
+++ Digital Signature/Add-digital-signature-with-digest-algorithm/.NET/Add-digital-signature-with-digest-algorithm/Program.cs
@@ -1,51 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Syncfusion.Drawing;
+﻿using Syncfusion.Drawing;
 using Syncfusion.Pdf;
 using Syncfusion.Pdf.Graphics;
 using Syncfusion.Pdf.Security;
 
 //Creates a new PDF document.
-PdfDocument document = new PdfDocument();
-
-//Adds a new page.
-PdfPageBase page = document.Pages.Add();
-
-//Create graphics with the page. 
-PdfGraphics graphics = page.Graphics;
-
-//Creates a certificate instance from PFX file with private key.
-FileStream certificateStream = new FileStream(Path.GetFullPath(@"Data/PDF.pfx"), FileMode.Open, FileAccess.Read);
-PdfCertificate pdfCert = new PdfCertificate(certificateStream, "syncfusion");
-
-//Creates a digital signature.
-PdfSignature signature = new PdfSignature(document, page, pdfCert, "Signature");
-
-//Sets signature settings to customize digest algorithm specified.
-PdfSignatureSettings settings = signature.Settings;
-settings.DigestAlgorithm = DigestAlgorithm.SHA256;
-
-//Sets an image for signature field.
-FileStream imageStream = new FileStream(Path.GetFullPath(@"Data/signature.png"), FileMode.Open, FileAccess.Read);
-
-//Sets an image for signature field.
-PdfBitmap signatureImage = new PdfBitmap(imageStream);
-
-//Sets signature information.
-signature.Bounds = new RectangleF(new PointF(0, 0), signatureImage.PhysicalDimension);
-signature.ContactInfo = "johndoe@owned.us";
-signature.LocationInfo = "Honolulu, Hawaii";
-signature.Reason = "I am author of this document.";
-
-//Draws the signature image.
-signature.Appearance.Normal.Graphics.DrawImage(signatureImage, 0, 0);
-
-//Create file stream.
-using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+using (PdfDocument document = new PdfDocument())
 {
-    //Save the PDF document to file stream.
-    document.Save(outputFileStream);
-}
 
-//Close the document. 
-document.Close(true);
+    //Adds a new page.
+    PdfPageBase page = document.Pages.Add();
+
+    //Create graphics with the page. 
+    PdfGraphics graphics = page.Graphics;
+
+    //Creates a certificate instance from PFX file with private key.
+    FileStream certificateStream = new FileStream(Path.GetFullPath(@"Data/PDF.pfx"), FileMode.Open, FileAccess.Read);
+    PdfCertificate pdfCert = new PdfCertificate(certificateStream, "syncfusion");
+
+    //Creates a digital signature.
+    PdfSignature signature = new PdfSignature(document, page, pdfCert, "Signature");
+
+    //Sets signature settings to customize digest algorithm specified.
+    PdfSignatureSettings settings = signature.Settings;
+    settings.DigestAlgorithm = DigestAlgorithm.SHA256;
+
+    //Sets an image for signature field.
+    FileStream imageStream = new FileStream(Path.GetFullPath(@"Data/signature.png"), FileMode.Open, FileAccess.Read);
+
+    //Sets an image for signature field.
+    PdfBitmap signatureImage = new PdfBitmap(imageStream);
+
+    //Sets signature information.
+    signature.Bounds = new RectangleF(new PointF(0, 0), signatureImage.PhysicalDimension);
+    signature.SignedName = "Syncfusion";
+    signature.ContactInfo = "johndoe@owned.us";
+    signature.LocationInfo = "Honolulu, Hawaii";
+    signature.Reason = "I am author of this document.";
+
+    //Draws the signature image.
+    signature.Appearance.Normal.Graphics.DrawImage(signatureImage, 0, 0);
+
+    // Save the PDF document
+    document.Save(Path.GetFullPath(@"Output/Output.pdf"));
+
+}
--- Digital Signature/Adding-a-timestamp-in-digital-signature-of-PDF/.NET/Adding-a-timestamp-in-digital-signature-of-PDF/Program.cs
+++ Digital Signature/Adding-a-timestamp-in-digital-signature-of-PDF/.NET/Adding-a-timestamp-in-digital-signature-of-PDF/Program.cs
@@ -1,54 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Syncfusion.Drawing;
+﻿using Syncfusion.Drawing;
 using Syncfusion.Pdf;
 using Syncfusion.Pdf.Graphics;
 using Syncfusion.Pdf.Security;
 
 //Creates a new PDF document.
-PdfDocument document = new PdfDocument();
-
-//Adds a new page.
-PdfPageBase page = document.Pages.Add();
-
-//Create graphics for the page. 
-PdfGraphics graphics = page.Graphics;
-
-//Creates a certificate instance from PFX file with private key.
-FileStream certificateStream = new FileStream(Path.GetFullPath(@"Data/PDF.pfx"), FileMode.Open, FileAccess.Read);
-PdfCertificate pdfCert = new PdfCertificate(certificateStream, "syncfusion");
-
-//Creates a digital signature.
-PdfSignature signature = new PdfSignature(document, page, pdfCert, "Signature");
-
-//Change the digital signature standard and hashing algorithm.
-signature.Settings.CryptographicStandard = CryptographicStandard.CADES;
-signature.Settings.DigestAlgorithm = DigestAlgorithm.SHA512;
-
-//Sets an image for signature field.
-FileStream imageStream = new FileStream(Path.GetFullPath(@"Data/syncfusion_logo.png"), FileMode.Open, FileAccess.Read);
-
-//Sets an image for signature field.
-PdfBitmap image = new PdfBitmap(imageStream);
-
-//Adds time stamp by using the server URI and credentials.
-signature.TimeStampServer = new TimeStampServer(new Uri("http://time.certum.pl/"));
-
-//Sets signature info.
-signature.Bounds = new RectangleF(new PointF(0, 0), image.PhysicalDimension);
-signature.ContactInfo = "johndoe@owned.us";
-signature.LocationInfo = "Honolulu, Hawaii";
-signature.Reason = "I am author of this document.";
-
-//Draws the signature image.
-signature.Appearance.Normal.Graphics.DrawImage(image, 0, 0);
-
-//Create file stream.
-using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.ReadWrite))
+using (PdfDocument document = new PdfDocument())
 {
-    //Save the PDF document to file stream.
-    document.Save(outputFileStream);
-}
+    //Adds a new page.
+    PdfPageBase page = document.Pages.Add();
 
-//Close the document.
-document.Close(true);
+    //Create graphics for the page. 
+    PdfGraphics graphics = page.Graphics;
+
+    //Creates a certificate instance from PFX file with private key.
+    FileStream certificateStream = new FileStream(Path.GetFullPath(@"Data/PDF.pfx"), FileMode.Open, FileAccess.Read);
+    PdfCertificate pdfCert = new PdfCertificate(certificateStream, "syncfusion");
+
+    //Creates a digital signature.
+    PdfSignature signature = new PdfSignature(document, page, pdfCert, "Signature");
+
+    //Change the digital signature standard and hashing algorithm.
+    signature.Settings.CryptographicStandard = CryptographicStandard.CADES;
+    signature.Settings.DigestAlgorithm = DigestAlgorithm.SHA512;
+
+    //Sets an image for signature field.
+    FileStream imageStream = new FileStream(Path.GetFullPath(@"Data/syncfusion_logo.png"), FileMode.Open, FileAccess.Read);
+
+    //Sets an image for signature field.
+    PdfBitmap image = new PdfBitmap(imageStream);
+
+    //Adds time stamp by using the server URI and credentials.
+    signature.TimeStampServer = new TimeStampServer(new Uri("http://time.certum.pl/"));
+
+    //Sets signature info.
+    signature.Bounds = new RectangleF(new PointF(0, 0), image.PhysicalDimension);
+    signature.SignedName = "Syncfusion";
+    signature.ContactInfo = "johndoe@owned.us";
+    signature.LocationInfo = "Honolulu, Hawaii";
+    signature.Reason = "I am author of this document.";
+
+    //Draws the signature image.
+    signature.Appearance.Normal.Graphics.DrawImage(image, 0, 0);
+
+    // Save the PDF document
+    document.Save(Path.GetFullPath(@"Output/Output.pdf"));
+
+}
