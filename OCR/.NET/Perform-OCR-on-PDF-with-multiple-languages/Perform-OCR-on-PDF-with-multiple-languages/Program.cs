bcbb192998a6b4f11431d93fc2d195f4996249f8
sameerkhan001 <sameerkhan.n@syncfusion.com>
2025-11-12 15:54:40 +0530

    263124: Added OCR for multiple language in same scanned PDF document

---
+++ OCR/.NET/Perform-OCR-on-PDF-with-multiple-languages/Perform-OCR-on-PDF-with-multiple-languages/Program.cs
@@ -0,0 +1,20 @@
+﻿using Syncfusion.OCRProcessor;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Parsing;
+
+// Load the PDF document
+using (PdfLoadedDocument loadedDocument = new PdfLoadedDocument(Path.GetFullPath(@"Input.pdf")))
+{
+    // Initialize OCR processor
+    OCRProcessor processor = new OCRProcessor();
+    //Sets Unicode font to preserve the Unicode characters in a PDF document.
+    processor.UnicodeFont = new PdfTrueTypeFont(Path.GetFullPath(@"Data/ARIALUNI.ttf"), 8);
+    // Set OCR language
+    processor.Settings.Language = "eng+tha";
+    // Set the path to the Tesseract language data folder
+    processor.TessDataPath = Path.GetFullPath(@"../../Tessdata");
+    // Perform OCR
+    processor.PerformOCR(loadedDocument);
+    // Save the PDF document
+    loadedDocument.Save(Path.GetFullPath(@"Output/Output.pdf"));
+}
