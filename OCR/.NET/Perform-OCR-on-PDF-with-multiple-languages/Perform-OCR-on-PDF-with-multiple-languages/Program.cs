eb883977cba0b920bc8976dc4756e41305867319
sameerkhan001 <sameerkhan.n@syncfusion.com>
2025-11-13 10:31:34 +0530

    263124: Added proper code example also removed duplicates.

--- OCR/.NET/Perform-OCR-on-PDF-with-multiple-languages/Perform-OCR-on-PDF-with-multiple-languages/Program.cs
+++ OCR/.NET/Perform-OCR-on-PDF-with-multiple-languages/Perform-OCR-on-PDF-with-multiple-languages/Program.cs
@@ -3,14 +3,14 @@ using Syncfusion.Pdf.Graphics;
 using Syncfusion.Pdf.Parsing;
 
 // Load the PDF document
-using (PdfLoadedDocument loadedDocument = new PdfLoadedDocument(Path.GetFullPath(@"Input.pdf")))
+using (PdfLoadedDocument loadedDocument = new PdfLoadedDocument(Path.GetFullPath(@"Data/Input.pdf")))
 {
     // Initialize OCR processor
     OCRProcessor processor = new OCRProcessor();
     //Sets Unicode font to preserve the Unicode characters in a PDF document.
     processor.UnicodeFont = new PdfTrueTypeFont(Path.GetFullPath(@"Data/ARIALUNI.ttf"), 8);
     // Set OCR language
-    processor.Settings.Language = "eng+tha";
+    processor.Settings.Language = "eng+deu+ara+ell+fra"; // English, German, Arabic, Greek, French
     // Set the path to the Tesseract language data folder
     processor.TessDataPath = Path.GetFullPath(@"../../Tessdata");
     // Perform OCR
