fec62db9a7c942523b7ef30062382d44b205dbc2
sameerkhan001 <sameerkhan.n@syncfusion.com>
2025-11-25 12:34:52 +0530

    994279: Resolved the given feedback.

--- HTML to PDF/Blink/Skip-cover-page-in-TOC-page-numbering/.NET/Skip-cover-page-in-TOC-page-numbering/Program.cs
+++ HTML to PDF/Blink/Skip-cover-page-in-TOC-page-numbering/.NET/Skip-cover-page-in-TOC-page-numbering/Program.cs
@@ -2,10 +2,10 @@
 using Syncfusion.HtmlConverter;
 using Syncfusion.Pdf;
 
-// Create an instance of HTML-to-PDF converter using Blink rendering engine
+// Create an instance of HTML-to-PDF converter
 HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
 // Configure Blink converter settings
-BlinkConverterSettings blinkConverterSettings = new BlinkConverterSettings
+BlinkConverterSettings blinkConverterSettings = new BlinkConverterSettings()
 {
     ViewPortSize = new Size(1280, 0), // Set viewport width for rendering
     EnableToc = true,                 // Enable Table of Contents (TOC)
@@ -16,20 +16,21 @@ blinkConverterSettings.Toc.StartingPageNumber = 1;
 htmlConverter.ConverterSettings = blinkConverterSettings;
 // Read the main HTML content and convert it to PDF
 string inputhtml = File.ReadAllText(Path.GetFullPath(@"Data/input.html"));
-using (PdfDocument document = htmlConverter.Convert(inputhtml, ""))
+PdfDocument document = htmlConverter.Convert(inputhtml, "");
+//Create cover page and insert to the 0th index.
+// Apply scaling settings for the cover page
+htmlConverter.ConverterSettings = new BlinkConverterSettings()
 {
-    // Apply scaling settings for the cover page
-    htmlConverter.ConverterSettings = new BlinkConverterSettings()
-    {
-        Scale = 1.5f
-    };
-    // Convert the cover page HTML to PDF
-    string coverimage = File.ReadAllText(Path.GetFullPath(@"Data/coverpage.html"));
-    using (PdfDocument coverPage = htmlConverter.Convert(coverimage, ""))
-    {
-        // Insert the cover page at the beginning of the main document
-        document.Pages.Insert(0, coverPage.Pages[0]);
-        // Save the PDF document
-        document.Save(Path.GetFullPath(@"Output/Output.pdf"));
-    }
-}
+    Scale = 1.5f
+};
+// Convert the cover page HTML to PDF
+string coverimage = File.ReadAllText(Path.GetFullPath(@"Data/coverpage.html"));
+PdfDocument coverPage = htmlConverter.Convert(coverimage, "");
+// Insert the cover page at the beginning of the main document
+document.Pages.Insert(0, coverPage.Pages[0]);
+// Save the PDF document
+document.Save(Path.GetFullPath(@"Output/Output.pdf"));
+//Dispose the document
+coverPage.Close(true);
+document.Close(true);
+htmlConverter.Close();
