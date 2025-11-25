7c506af2750269991086250a3346c3afa642b4dd
sameerkhan001 <sameerkhan.n@syncfusion.com>
2025-11-25 10:37:32 +0530

    994279: Resolved the given feedback.

--- HTML to PDF/Blink/Skip-cover-page-in-TOC-page-numbering/.NET/Skip-cover-page-in-TOC-page-numbering/Program.cs
+++ HTML to PDF/Blink/Skip-cover-page-in-TOC-page-numbering/.NET/Skip-cover-page-in-TOC-page-numbering/Program.cs
@@ -16,18 +16,20 @@ blinkConverterSettings.Toc.StartingPageNumber = 1;
 htmlConverter.ConverterSettings = blinkConverterSettings;
 // Read the main HTML content and convert it to PDF
 string inputhtml = File.ReadAllText(Path.GetFullPath(@"Data/input.html"));
-PdfDocument document = htmlConverter.Convert(inputhtml, "");
-// Create new settings for scaling the cover page
-BlinkConverterSettings settings = new BlinkConverterSettings();
-settings.Scale = 1.5f;
-// Apply scaling settings
-htmlConverter.ConverterSettings = settings;
-// Convert the cover page HTML to PDF
-string coverimage = File.ReadAllText(Path.GetFullPath(@"Data/coverpage.html"));
-PdfDocument coverPage = htmlConverter.Convert(coverimage, "");
-// Insert the cover page at the beginning of the main document
-document.Pages.Insert(0, coverPage.Pages[0]);
-// Save the PDF document
-document.Save(Path.GetFullPath(@"Output/Output.pdf"));
-// Close the PDF document
-document.Close(true);
+using (PdfDocument document = htmlConverter.Convert(inputhtml, ""))
+{
+    // Apply scaling settings for the cover page
+    htmlConverter.ConverterSettings = new BlinkConverterSettings()
+    {
+        Scale = 1.5f
+    };
+    // Convert the cover page HTML to PDF
+    string coverimage = File.ReadAllText(Path.GetFullPath(@"Data/coverpage.html"));
+    using (PdfDocument coverPage = htmlConverter.Convert(coverimage, ""))
+    {
+        // Insert the cover page at the beginning of the main document
+        document.Pages.Insert(0, coverPage.Pages[0]);
+        // Save the PDF document
+        document.Save(Path.GetFullPath(@"Output/Output.pdf"));
+    }
+}
