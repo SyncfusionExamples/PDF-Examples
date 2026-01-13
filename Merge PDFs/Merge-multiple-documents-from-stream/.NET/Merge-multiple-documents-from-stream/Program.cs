be146478b874efcda796fb90a099d49e82bd1d2f
jeyalakshmit <jeyalakshmi.t@syncfusion.com>
2026-01-13 16:15:40 +0530

    1002794: Updated UG documentation with File overload support for Cross platform

--- Merge PDFs/Merge-multiple-documents-from-stream/.NET/Merge-multiple-documents-from-stream/Program.cs
+++ Merge PDFs/Merge-multiple-documents-from-stream/.NET/Merge-multiple-documents-from-stream/Program.cs
@@ -3,12 +3,11 @@
 //Creates a PDF document.
 using (PdfDocument document = new PdfDocument())
 {
-    using FileStream stream1 = new FileStream(Path.GetFullPath(@"Data/file1.pdf"), FileMode.Open, FileAccess.Read);
-    using FileStream stream2 = new FileStream(Path.GetFullPath(@"Data/file2.pdf"), FileMode.Open, FileAccess.Read);
-    //Creates a PDF stream for merging.
-    Stream[] streams = { stream1, stream2 };
+    //Creates a string array of source files to be merged.
+    string[] source = { Path.GetFullPath(@"Data/file1.pdf"), Path.GetFullPath(@"Data/file2.pdf") };
     //Merges PDFDocument.
-    PdfDocumentBase.Merge(document, streams);
+    PdfDocumentBase.Merge(document, source);
     //Save the merged document
     document.Save(Path.GetFullPath(@"Output/Output.pdf"));
+    document.Close(true);
 }
