cb40de742e760d00393052f306e19f40174152bf
sameerkhan001 <sameerkhan.n@syncfusion.com>
2026-01-14 18:40:26 +0530

    1004110: Resolved given feedback.

--- Forms/Add-a-date-field-to-a-PDF/.NET/Add-a-date-field-to-a-PDF/Program.cs
+++ Forms/Add-a-date-field-to-a-PDF/.NET/Add-a-date-field-to-a-PDF/Program.cs
@@ -9,13 +9,13 @@ using (PdfDocument pdfDocument = new PdfDocument())
     PdfPage pdfPage = pdfDocument.Pages.Add();
     // Create a text box field for entering the name
     PdfTextBoxField nameField = new PdfTextBoxField(pdfPage, "DateField");
-    nameField.Bounds = new RectangleF(10, 40, 200, 20);
+    nameField.Bounds = new RectangleF(10, 40, 70, 20);
     nameField.ToolTip = "Date";
     nameField.Text = "12/01/1995";
     //Set the scriptAction to submitButton
-    nameField.Actions.KeyPressed = new PdfJavaScriptAction("AFDate_KeystrokeEx(\"m/d/yy\")");
-    nameField.Actions.Format = new PdfJavaScriptAction("AFDate_FormatEx(\"m/d/yy\")");
-    nameField.Actions.Validate = new PdfJavaScriptAction("AFDate_Validate(\"m/d/yy\")");
+    nameField.Actions.KeyPressed = new PdfJavaScriptAction("AFDate_KeystrokeEx(\"mm/dd/yyyy\")");
+    nameField.Actions.Format = new PdfJavaScriptAction("AFDate_FormatEx(\"mm/dd/yyyy\")");
+    nameField.Actions.Validate = new PdfJavaScriptAction("AFDate_Validate(\"mm/dd/yyyy\")");
     // Add the field to the form
     pdfDocument.Form.Fields.Add(nameField);
     // Save the PDF document
