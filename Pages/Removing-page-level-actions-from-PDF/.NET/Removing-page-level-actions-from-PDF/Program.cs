8bbb97295048c66e1e3bcb64fe2353d595804c37
sameerkhan001 <sameerkhan.n@syncfusion.com>
2026-03-19 12:39:03 +0530

    1016957:Resolved the given feedback.

--- Pages/Removing-page-level-actions-from-PDF/.NET/Removing-page-level-actions-from-PDF/Program.cs
+++ Pages/Removing-page-level-actions-from-PDF/.NET/Removing-page-level-actions-from-PDF/Program.cs
@@ -1,41 +1,21 @@
 ﻿using Syncfusion.Pdf;
 using Syncfusion.Pdf.Interactive;
 
-// Create a new PDF document.
-using (PdfDocument document = new PdfDocument())
+// Load the existing PDF document
+using (PdfLoadedDocument document = new PdfLoadedDocument(
+           Path.GetFullPath(@"Data/Input.pdf")))
 {
-    // Add a page to the document.
-    PdfPage page1 = document.Pages.Add();
-    // Create and add new JavaScript action to execute when the first page opens
-    page1.Actions.OnOpen = new PdfJavaScriptAction("app.alert(\"Welcome! This page has just been opened.\");");
-    // Create and add new URI action to execute when the first page closes
-    page1.Actions.OnClose = new PdfUriAction("http://www.google.com");
-    // Add second page to the document.
-    PdfPage page2 = document.Pages.Add();
-    // Create a sound action 
-    PdfSoundAction soundAction = new PdfSoundAction(Path.GetFullPath(@"Data/Startup.wav"));
-    soundAction.Sound.Bits = 16;
-    soundAction.Sound.Channels = PdfSoundChannels.Stereo;
-    soundAction.Sound.Encoding = PdfSoundEncoding.Signed;
-    soundAction.Volume = 0.9f;
-    // Set the so und action to execute when the second page opens
-    page2.Actions.OnOpen = soundAction;
-    // Create and add new Launch action to execute when the second page closes
-    page2.Actions.OnClose = new PdfLaunchAction(Path.GetFullPath(@"Data/logo.png"));
-    // Add third page to the document
-    PdfPage page3 = document.Pages.Add();
-    // Create and add new JavaScript action to execute when the third page opens
-    PdfAction jsAction = new PdfJavaScriptAction("app.alert(\"Welcome! Third page has just been opened.\");");
-    jsAction.Next = new PdfJavaScriptAction("app.alert(\"This is the second action.\");");
-    jsAction.Next.Next = new PdfJavaScriptAction("app.alert(\"This is the third action.\");");
-    page3.Actions.OnOpen = jsAction;
-    // Removing the open action on first page
-    page1.Actions.OnOpen = null;
-    // Removing the close action on second page
-    page2.Actions.OnClose = null;
-    // Removing both actions on third page 
-    page3.Actions.Clear(false);
-    //Save the document
+    // Iterate through all pages in the document
+    foreach (PdfLoadedPage page in document.Pages)
+    {
+        // Remove any JavaScript or actions that execute
+        // when the page is opened
+        page.Actions.OnOpen = null;
+        // Remove any JavaScript or actions that execute
+        // when the page is closed
+        page.Actions.OnClose = null;
+    }
+    // Save the modified PDF document
     document.Save(Path.GetFullPath(@"Output/Output.pdf"));
     // Close the document
     document.Close(true);
