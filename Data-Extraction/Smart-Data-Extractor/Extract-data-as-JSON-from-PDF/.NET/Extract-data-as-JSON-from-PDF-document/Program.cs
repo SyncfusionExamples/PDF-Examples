64b181bb7c73bd4e5c66e2041905978f3cb4fcbf
venkateshwaransf5013 <venkateshwaran.k@syncfusion.com>
2026-04-06 16:56:38 +0530

    Added the samples for data extractor

---
+++ Data-Extraction/Smart-Data-Extractor/Extract-data-as-JSON-from-PDF/.NET/Extract-data-as-JSON-from-PDF-document/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+using Syncfusion.SmartDataExtractor;
+
+namespace ExtractDataAsJsonFromPdfDocument
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Open the input PDF file as a stream.
+			using (FileStream stream = new FileStream(@"Data\Input.pdf", FileMode.Open, FileAccess.Read))
+			{
+				// Initialize the Smart Data Extractor.
+				DataExtractor extractor = new DataExtractor();
+
+				// Extract form data as JSON.
+				string data = extractor.ExtractDataAsJson(stream);
+
+				// Save the extracted JSON data into an output file.
+				File.WriteAllText(@"Output\Output.json", data, Encoding.UTF8);
+			}
+		}
+	}
+}
---
+++ Data-Extraction/Smart-Data-Extractor/Extract-data-from-PDF-document/.NET/Extract-data-from-PDF-document/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using Syncfusion.Pdf.Parsing;
+using Syncfusion.SmartDataExtractor;
+
+namespace ExtractDataFromPDFDocument
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Open the input PDF file as a stream.
+			using (FileStream inputStream = new FileStream(Path.GetFullPath(@"Data\Input.pdf"), FileMode.Open))
+			{
+				//Initialize the Smart Data Extractor.
+				DataExtractor extractor = new DataExtractor();
+				//Extract data and return as a loaded PDF document.
+				PdfLoadedDocument document = extractor.ExtractDataAsPdfDocument(inputStream);
+
+				//Save the extracted output as a new PDF file inside the Output folder.
+				document.Save(Path.GetFullPath(@"Output/Output.pdf"));
+
+				//Close the document to release resources.
+				document.Close(true);
+			}
+		}
+	}
+}
