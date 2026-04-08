9c4d723cd0b4c26d1889e53c7234a68345a52fa9
venkateshwaransf5013 <venkateshwaran.k@syncfusion.com>
2026-04-08 11:49:59 +0530

    Added the samples for smart form recoginizer

---
+++ Data-Extraction/Smart-Form-Recognizer/Recognize-forms-using-JSON-async/.NET/Recognize-forms-using-JSON-async/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Syncfusion.SmartFormRecognizer;
+
+namespace RecognizeFormsUsingJsonAsync
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			// Read the input PDF file as stream.
+			using (FileStream inputStream = new FileStream(Path.GetFullPath(@"Data\Input.pdf"), FileMode.Open, FileAccess.ReadWrite))
+			{
+				// Initialize the Form Recognizer.
+				FormRecognizer smartFormRecognizer = new FormRecognizer();
+				// Recognize the form and get the output as JSON string asynchronously.
+				string outputJson = await smartFormRecognizer.RecognizeFormAsJsonAsync(inputStream);
+				// Save the output JSON to file.
+				File.WriteAllText(Path.GetFullPath(@"Output\Output.json"), outputJson);
+			}
+		}
+	}
+}
---
+++ Data-Extraction/Smart-Form-Recognizer/Recognize-forms-using-Stream/.NET/Recognize-forms-using-Stream/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Syncfusion.SmartFormRecognizer;
+
+namespace RecognizeFormsUsingStream
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Read the input PDF file as stream.
+			using (FileStream inputStream = new FileStream(Path.GetFullPath(@"Data\Input.pdf"), FileMode.Open, FileAccess.ReadWrite))
+			{
+				// Initialize the Form Recognizer.
+				FormRecognizer smartFormRecognizer = new FormRecognizer();
+				// Recognize the form and get the output as PDF stream.
+				Stream outputStream = smartFormRecognizer.RecognizeFormAsPdfStream(inputStream);
+				// Save the output PDF stream to file.
+				using (FileStream fileStream = File.Create(Path.GetFullPath(@"Output\Output.pdf")))
+				{
+					outputStream.Seek(0, SeekOrigin.Begin);
+					outputStream.CopyTo(fileStream);
+				}
+			}
+		}
+	}
+}
