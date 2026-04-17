0efeb4261c0a3a68344957501d6cf596b50add7f
venkateshwaransf5013 <venkateshwaran.k@syncfusion.com>
2026-04-17 16:46:52 +0530

    Added the changes

--- Data-Extraction/Getting-Started/ASP.NETCore/Recognize_Forms/Recognize_Forms/Models/ErrorViewModel.cs
+++ Data-Extraction/Getting-Started/ASP.NETCore/Recognize_Forms/Recognize_Forms/Models/ErrorViewModel.cs
@@ -1,9 +1,9 @@
 namespace Recognize_Forms.Models
 {
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
--- Data-Extraction/Getting-Started/ASP.NETCore/Recognize_Forms/Recognize_Forms/Program.cs
+++ Data-Extraction/Getting-Started/ASP.NETCore/Recognize_Forms/Recognize_Forms/Program.cs
@@ -8,9 +8,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -21,7 +21,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
