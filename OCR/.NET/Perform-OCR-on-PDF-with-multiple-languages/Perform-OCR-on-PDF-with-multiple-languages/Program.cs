147132f81302b3fcbb20028e32d34ac4362699f8
sameerkhan001 <sameerkhan.n@syncfusion.com>
2026-07-28 19:33:42 +0530

    1043933: Resolved the playground failures

--- OCR/.NET/Perform-OCR-on-PDF-with-multiple-languages/Perform-OCR-on-PDF-with-multiple-languages/Program.cs
+++ OCR/.NET/Perform-OCR-on-PDF-with-multiple-languages/Perform-OCR-on-PDF-with-multiple-languages/Program.cs
@@ -10,11 +10,9 @@ using (PdfLoadedDocument loadedDocument = new PdfLoadedDocument(Path.GetFullPath
     //Sets Unicode font to preserve the Unicode characters in a PDF document.
     processor.UnicodeFont = new PdfTrueTypeFont(Path.GetFullPath(@"Data/ARIALUNI.ttf"), 8);
     // Set OCR language
-    processor.Settings.Language = "eng+deu+ara+ell+fra"; // English, German, Arabic, Greek, French
-    // Set the path to the Tesseract language data folder
-    processor.TessDataPath = Path.GetFullPath(@"../../Tessdata");
+    processor.Settings.Language = "eng+deu+ara+ell"; // English, German, Arabic, Greek
     // Perform OCR
-    processor.PerformOCR(loadedDocument);
+    processor.PerformOCR(loadedDocument, processor.TessDataPath);
     // Save the PDF document
     loadedDocument.Save(Path.GetFullPath(@"Output/Output.pdf"));
 }
