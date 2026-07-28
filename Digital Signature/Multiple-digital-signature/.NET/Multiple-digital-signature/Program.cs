28372ee884648a6f59e7df1cdefe10c652e34a38
sameerkhan001 <sameerkhan.n@syncfusion.com>
2026-07-28 20:53:50 +0530

    1043933-1: Updated proper code.

--- Digital Signature/Multiple-digital-signature/.NET/Multiple-digital-signature/Program.cs
+++ Digital Signature/Multiple-digital-signature/.NET/Multiple-digital-signature/Program.cs
@@ -4,47 +4,42 @@ using Syncfusion.Pdf.Security;
 using Syncfusion.Pdf;
 
 //Load an existing PDF document. 
-using (PdfLoadedDocument loadedDocument = new PdfLoadedDocument(Path.GetFullPath(@"Data/SignatureFields.pdf")))
-{
-
-    //Get the first page of the document.
-    PdfLoadedPage page = loadedDocument.Pages[0] as PdfLoadedPage;
-    //Get the first signature field of the PDF document.
-    PdfLoadedSignatureField signatureField1 = loadedDocument.Form.Fields[0] as PdfLoadedSignatureField;
-    //Create a certificate instance from a PFX file with a private key.
-    FileStream certificateStream = new FileStream(Path.GetFullPath(@"Data/PDF.pfx"), FileMode.Open, FileAccess.Read);
-    PdfCertificate certificate1 = new PdfCertificate(certificateStream, "syncfusion");
-    //Add a signature to the signature field.
-    signatureField1.Signature = new PdfSignature(loadedDocument, page, certificate1, "Signature", signatureField1);
-    //Set an image for the signature field.
-    FileStream imageStream1 = new FileStream(Path.GetFullPath(@"Data/Student Signature.jpg"), FileMode.Open, FileAccess.Read);
-    PdfBitmap signatureImage = new PdfBitmap(imageStream1);
-    //Insert an image in the signature appearance. 
-    signatureField1.Signature.Appearance.Normal.Graphics.DrawImage(signatureImage, 0, 0, 90, 20);
-    //Save the document into the stream.
-    MemoryStream stream = new MemoryStream();
-    loadedDocument.Save(stream);
-
-    //Load the signed PDF document.
-    using (PdfLoadedDocument signedDocument = new PdfLoadedDocument(stream))
-    {
-        //Load the PDF page.
-        PdfLoadedPage loadedPage = signedDocument.Pages[0] as PdfLoadedPage;
-
-        //Get the first signature field of the PDF document.
-        PdfLoadedSignatureField signatureField2 = signedDocument.Form.Fields[1] as PdfLoadedSignatureField;
-
-        //Add a signature to the signature field. 
-        signatureField2.Signature = new PdfSignature(signedDocument, loadedPage, certificate1, "Signature", signatureField2);
-
-        //Set an image for the signature field.
-        FileStream imageStream2 = new FileStream(Path.GetFullPath(@"Data/Teacher Signature.png"), FileMode.Open, FileAccess.Read);
-        PdfBitmap signatureImage1 = new PdfBitmap(imageStream2);
-
-        //Draw an image in the signature appearance. 
-        signatureField2.Signature.Appearance.Normal.Graphics.DrawImage(signatureImage1, 0, 0, 90, 20);
-
-        //Save the PDF document
-        signedDocument.Save(Path.GetFullPath(@"Output/Output.pdf"));
-    }
-}
+PdfLoadedDocument loadedDocument = new PdfLoadedDocument(Path.GetFullPath(@"Data/SignatureFields.pdf"));
+//Get the first page of the document.
+PdfLoadedPage page = loadedDocument.Pages[0] as PdfLoadedPage;
+//Get the first signature field of the PDF document.
+PdfLoadedSignatureField signatureField1 = loadedDocument.Form.Fields[0] as PdfLoadedSignatureField;
+//Create a certificate instance from a PFX file with a private key.
+PdfCertificate certificate1 = new PdfCertificate(Path.GetFullPath(@"Data/PDF.pfx"), "syncfusion");
+//Add a signature to the signature field.
+signatureField1.Signature = new PdfSignature(loadedDocument, page, certificate1, "Signature", signatureField1);
+//Set an image for the signature field.
+FileStream imageStream1 = new FileStream(Path.GetFullPath(@"Data/Student Signature.jpg"), FileMode.Open, FileAccess.Read);
+PdfBitmap signatureImage = new PdfBitmap(imageStream1);
+imageStream1.Position = 0;
+//Insert an image in the signature appearance. 
+signatureField1.Signature.Appearance.Normal.Graphics.DrawImage(signatureImage, 0, 0, signatureField1.Bounds.Width, signatureField1.Bounds.Height);
+//Save the document into the stream.
+MemoryStream stream = new MemoryStream();
+loadedDocument.Save(stream);
+//Close the PDF document
+loadedDocument.Close(true);
+stream.Position = 0;
+//Load the signed PDF document.
+PdfLoadedDocument signedDocument = new PdfLoadedDocument(stream);
+//Load the PDF page.
+PdfLoadedPage loadedPage = signedDocument.Pages[0] as PdfLoadedPage;
+//Get the first signature field of the PDF document.
+PdfLoadedSignatureField signatureField2 = signedDocument.Form.Fields[1] as PdfLoadedSignatureField;
+//Add a signature to the signature field. 
+signatureField2.Signature = new PdfSignature(signedDocument, loadedPage, certificate1, "Signature", signatureField2);
+//Set an image for the signature field.
+FileStream imageStream2 = new FileStream(Path.GetFullPath(@"Data/Teacher Signature.png"), FileMode.Open, FileAccess.Read);
+PdfBitmap signatureImage1 = new PdfBitmap(imageStream2);
+imageStream2.Position = 0;
+//Draw an image in the signature appearance. 
+signatureField2.Signature.Appearance.Normal.Graphics.DrawImage(signatureImage1, 0, 0, signatureField2.Bounds.Width, signatureField2.Bounds.Height);
+//Save the PDF document
+signedDocument.Save(Path.GetFullPath(@"Output/Output.pdf"));
+//Close the PDF document
+signedDocument.Close(true);
--- OCR/.NET/Perform-OCR-on-PDF-with-multiple-languages/Perform-OCR-on-PDF-with-multiple-languages/Program.cs
+++ OCR/.NET/Perform-OCR-on-PDF-with-multiple-languages/Perform-OCR-on-PDF-with-multiple-languages/Program.cs
@@ -10,9 +10,11 @@ using (PdfLoadedDocument loadedDocument = new PdfLoadedDocument(Path.GetFullPath
     //Sets Unicode font to preserve the Unicode characters in a PDF document.
     processor.UnicodeFont = new PdfTrueTypeFont(Path.GetFullPath(@"Data/ARIALUNI.ttf"), 8);
     // Set OCR language
-    processor.Settings.Language = "eng+deu+ara+ell"; // English, German, Arabic, Greek
+    processor.Settings.Language = "eng+deu+ara+ell+fra"; // English, German, Arabic, Greek, French
+    // Set the path to the Tesseract language data folder
+    processor.TessDataPath = Path.GetFullPath(@"Tessdata");
     // Perform OCR
-    processor.PerformOCR(loadedDocument, processor.TessDataPath);
+    processor.PerformOCR(loadedDocument);
     // Save the PDF document
     loadedDocument.Save(Path.GetFullPath(@"Output/Output.pdf"));
 }
