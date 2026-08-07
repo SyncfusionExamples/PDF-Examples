5647a33f74301c675a074cf69704cb230559b7f8
sameerkhan001 <sameerkhan.n@syncfusion.com>
2026-08-07 11:58:19 +0530

    1046084: Added missing sample project

---
+++ Annotation/Retrieve-Custom-PDF-Annotation-Values/.NET/Retrieve-Custom-PDF-Annotation-Values/Program.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.Pdf;
+using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf.Parsing;
+
+// Load the PDF document
+using (PdfLoadedDocument loadedDocument = new PdfLoadedDocument(Path.GetFullPath(@"Data/Input.pdf")))
+{
+    // Get the first page of the document
+    PdfLoadedPage page = loadedDocument.Pages[0] as PdfLoadedPage;
+    // Get the annotation collection from the page
+    PdfLoadedAnnotationCollection annotations = page.Annotations;
+    // Get the first annotation (assumed to be a rectangle annotation)
+    PdfLoadedRectangleAnnotation annot = annotations[8] as PdfLoadedRectangleAnnotation;
+    // Try to get the custom value "Subtype" from the annotation
+    object values;
+    bool foundValue = annot.TryGetValue("Subtype", out values);
+    // Check and print the values if found
+    if (foundValue && values is List<string> stringValues)
+    {
+        foreach (string value in stringValues)
+        {
+            // Print the custom value to the console
+            Console.WriteLine($"Found Subtype value: {value}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Subtype value not found.");
+    }
+}
