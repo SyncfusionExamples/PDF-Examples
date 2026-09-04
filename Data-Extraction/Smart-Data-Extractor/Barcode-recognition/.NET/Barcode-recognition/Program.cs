c886d90d4f98c7338adc62f07dc523f822681420
venkateshwaransf5013 <venkateshwaran.k@syncfusion.com>
2026-09-04 16:45:13 +0530

    Added samples for Barcode-recognition and PDf to word in Data extraction

---
+++ Data-Extraction/Smart-Data-Extractor/Barcode-recognition/.NET/Barcode-recognition/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using Syncfusion.SmartDataExtractor;
+
+//Open the input PDF file as a stream.
+using (FileStream stream = new FileStream(Path.GetFullPath(@"Data/Input.pdf"), FileMode.Open, FileAccess.Read))
+{
+	//Initialize the Data Extractor.
+	DataExtractor extractor = new DataExtractor();
+	//Extract data as JSON.
+	string data = extractor.ExtractDataAsJson(stream);
+	//Save the extracted JSON data into an output file.
+	File.WriteAllText(Path.GetFullPath(@"Output/Output.pdf"), data, Encoding.UTF8);
+}
